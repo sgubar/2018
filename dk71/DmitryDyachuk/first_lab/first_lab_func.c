da4bd1d1391be57df28e89fac1470ec40d08ac9b
I-N-G-U <36330618+I-N-G-U@users.noreply.github.com>
2018-02-12 19:40:34 +0200

    Update first_lab_func.c

--- dk71/DmitryDyachuk/first_lab/first_lab_func.c
+++ dk71/DmitryDyachuk/first_lab/first_lab_func.c
@@ -1,6 +1,6 @@
 #include "first_lab_func.h"
 
-int factorial(int factorial_value) // Функция по вычислению факториала из переменной B
+int factorial(int factorial_value) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ ГґГ ГЄГІГ®Г°ГЁГ Г«Г  ГЁГ§ ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г®Г© B
 {
     int output_value = 1;
     while(factorial_value != 1)
@@ -11,36 +11,38 @@ int factorial(int factorial_value) // Функция по вычислению факториала из переме
     return output_value;
 }
 
-int numerator(int numerator_A, int numerator_B) // Функция по вычислению операции сложения в числителе
+int numerator(int numerator_A, int numerator_B) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ Г®ГЇГҐГ°Г Г¶ГЁГЁ Г±Г«Г®Г¦ГҐГ­ГЁГї Гў Г·ГЁГ±Г«ГЁГІГҐГ«ГҐ
 {
     int value_A = second_degree(numerator_A);
     int value_B = second_degree(numerator_B);
     return value_A += value_B;
 }
 
-int second_degree(int value_degree) // Функция по вычислению квадарата из передаваемой переменной
+int second_degree(int value_degree) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ ГЄГўГ Г¤Г Г°Г ГІГ  ГЁГ§ ГЇГҐГ°ГҐГ¤Г ГўГ ГҐГ¬Г®Г© ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г®Г©
 {
     return value_degree *= value_degree;
 }
 
-int denominator(int denominator_B, int denominator_C) // Функция по вычислению операции сложения в знаменателе
+int denominator(int denominator_B, int denominator_C) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ Г®ГЇГҐГ°Г Г¶ГЁГЁ Г±Г«Г®Г¦ГҐГ­ГЁГї Гў Г§Г­Г Г¬ГҐГ­Г ГІГҐГ«ГҐ
 {
     int C_degree_of_5 = C_degree(denominator_C);
     return denominator_B + C_degree_of_5;
 }
 
-int C_degree(int C_value) // Функция по вычислению С-степени из числа 5
+int C_degree(int C_value) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ Г‘-Г±ГІГҐГЇГҐГ­ГЁ ГЁГ§ Г·ГЁГ±Г«Г  5
 {
     int result_of_C_degree = 5;
-    while(C_value > 1)
+    while(C_value >= 2)
     {
         result_of_C_degree *= 5;
         C_value--;
     }
+    if(C_value == 0)
+        result_of_C_degree *= 0;
     return result_of_C_degree;
 }
 
-float all_expression(int factorial_result, float numerator_result, float denominator_resut) // Функция по вычислению результата из всего выражения
+float all_expression(int factorial_result, float numerator_result, float denominator_resut) // Г”ГіГ­ГЄГ¶ГЁГї ГЇГ® ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГѕ Г°ГҐГ§ГіГ«ГјГІГ ГІГ  ГЁГ§ ГўГ±ГҐГЈГ® ГўГ»Г°Г Г¦ГҐГ­ГЁГї
 {
     float division_of_expression = numerator_result / denominator_resut;
     float sum_of_expression = division_of_expression + factorial_result;
