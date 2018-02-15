b45cdc392b617ce0464dc9cb8691f38e2e03580e
k561la3 <k561la3@meta.ua>
2018-02-15 21:35:58 +0000

    dz1

---
+++ dk72/raibu/hw1/main.c
@@ -0,0 +1,15 @@
+//main.c
+#include "dk_tool.h"
+
+
+
+int main(int argc, char const *argv[])
+{
+	printf("%i\n", get_int8()); 
+	return 0;
+}
+
+//123(8) = 3+16+64 = 83
+//1000 = 0+0*8+0*64+1*512
+
+
