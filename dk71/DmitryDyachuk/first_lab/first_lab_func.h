ae28b1efe91caf6d59681468802f76c711711747
I-N-G-U <36330618+I-N-G-U@users.noreply.github.com>
2018-02-18 00:45:21 +0200

    Update first_lab_func.h

--- dk71/DmitryDyachuk/first_lab/first_lab_func.h
+++ dk71/DmitryDyachuk/first_lab/first_lab_func.h
@@ -10,6 +10,6 @@
 int factorial(int factorial_value); // function for calculating output of factorial.
 int numerator(int numerator_A, int numerator_B); // function for calculating output of numerator.
 int second_degree(int value_degree); // function for calculation output of the second power.
-int C_degree(int C_value); // function for calculating output the C-th power/
+int C_degree(int C_value); // function for calculating output the C-th power.
 int denominator(int denominator_B, int denominator_C); // function for calculating output of denominator.
 float all_expression(int factorial_result, float numerator_result, float denominator_resut); // function for calculating output of all expression.
