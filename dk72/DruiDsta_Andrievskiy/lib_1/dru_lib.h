3474d2602aa417c5f9c84576f67a6d98d2b58cc3
DruiDsta <33695583+DruiDsta@users.noreply.github.com>
2018-02-21 19:03:01 +0200

    Create dru_lib.h

---
+++ dk72/DruiDsta_Andrievskiy/lib_1/dru_lib.h
@@ -0,0 +1,15 @@
+#ifndef dru_lib.h
+#define dru_lib.h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double algeb_summ_fac(double up, double down);
+
+double factorial(double fac);
+
+double drib(double a, double b, double c);
+
+double formula(double a, double b, double c, double d);
+
+#endif
