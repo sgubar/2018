723964308c63d506e5767d27567d547911335cea
NazarVolynko <36282087+NazarVolynko@users.noreply.github.com>
2018-02-21 20:37:03 +0200

    Create dk_tool.h

---
+++ dk72/NazarVolynko/HW1/dk_tool.h
@@ -0,0 +1,15 @@
+#ifndef DK_TOOL_H
+#define DK_TOOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+int* MatrixSum ( int *A , int *B ,int rows , int colums );
+
+void MatrixOutPut (int* M , int rows , int colums);
+
+void InputMatrix( int* M , int rows , int colums);
+
+#endif
