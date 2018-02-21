28c9d96e4b6acd33b63cf89dee338bb514be970c
NazarVolynko <36282087+NazarVolynko@users.noreply.github.com>
2018-02-21 20:39:04 +0200

    Add files via upload

---
+++ dk72/NazarVolynko/HW1/func.h
@@ -0,0 +1,15 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+int* MatrixSum ( int *A , int *B ,int rows , int colums );
+
+void MatrixOutPut (int* M , int rows , int colums);
+
+void InputMatrix( int* M , int rows , int colums);
+
+#endif
