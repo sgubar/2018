e82a734cefffae89524d2a612754b33491ab855c
yaroslav-dzhur <32819604+yaroslav-dzhur@users.noreply.github.com>
2018-02-22 14:25:37 +0200

    Add files via upload

---
+++ dk71/Yaroslav Dzhur/Laba1/dk_tool.h
@@ -0,0 +1,8 @@
+#ifndef HW1_DK_TOOL_H
+#define HW1_DK_TOOL_H
+
+#include <stdio.h>
+
+void subtractedmatrix(int n, int matrixC[n][n], int matrixA[n][n], int matrixB[n][n]);
+
+#endif //HW1_DK_TOOL_H
