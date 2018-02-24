20a59104cad56f5dc9f304c625edc3f62acfd6f4
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-02-24 16:14:15 +0200

    Create factorial.h

---
+++ dk72/Ny_Ok/factorial.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+SummaFaktorialov = 0;
+for(int D=0; D<=C; D++)
+    A=A+D;
+    int factorial = 1;   /*обновлённое значение факториала*/
+    for (int i = 1; i<=A; i++)/*вложенный цикл для факториала*/
+    {
+    /*printf( "i ==============%d\n", i);*/
+    factorial = factorial * i;
+    /*printf( "factorial==============%d\n", factorial);*/
+    }
+    /*printf( "D======%d\n", D );*/
+    SummaFaktorialov = SummaFaktorialov + factorial;
+    /*printf( "SummaFaktorialov===%d\n", SummaFaktorialov );*/
+    }
+    printf( "SummaFaktorialov===%d\n", SummaFaktorialov );
+return 0;
+}
