5ce7122d19bc40b8ca3b19a2cdc77d434ee5d1b9
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-02-24 16:15:23 +0200

    Update q.c

--- dk72/Ny_Ok/q.c
+++ dk72/Ny_Ok/q.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+{
+    float Q;
+    Q=A*Koren/B+SummaFaktorialov;
+    printf("******************************************************************************\n");
+    printf("Q===%.2f\n",Q);
+    return 0;
+}
 
