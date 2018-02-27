79500d31dec5577c2fc8fff05a2b5491533010b2
ihorbeliasnuk <dynamite588@gmail.com>
2018-02-27 19:27:07 +0200

    Create functions.h

---
+++ dk72/IhorBeliasnuk/LW1/functions.h
@@ -0,0 +1,12 @@
+#ifndef LW1_FUNCTIONS_H
+#define LW1_FUNCTIONS_H
+
+#include <stdio.h>
+
+float absolutValue(float a);
+
+float sum(float a);
+
+float exponentation(float val, int exp);
+
+#endif //LW1_FUNCTIONS_H
