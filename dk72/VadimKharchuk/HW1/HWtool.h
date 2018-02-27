6b14ae921bb8396963b93131957a5645d3f6d72c
M0rel <36273999+M0rel@users.noreply.github.com>
2018-02-27 17:39:47 +0200

    Add files via upload

---
+++ dk72/VadimKharchuk/HW1/HWtool.h
@@ -0,0 +1,15 @@
+//
+//H_W_tool.h
+//
+//hw1
+//
+//Created by Vadim Kharhuk on 2/09/18.
+//
+//Copyright © 2018 Vadim Kharchuk. All rights reserved.
+
+#ifndef HWtool_H
+#define HWtool_H
+
+void Seach(char str[]);
+
+#endif	 /* HWtool_h */
