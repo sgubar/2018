02448fe84626eceeabf24735358df78394729bfa
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-02-28 21:27:12 +0200

    Update factorial.h

--- dk72/Ny_Ok/factorial.h
+++ dk72/Ny_Ok/factorial.h
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-
-SummaFaktorialov = 0;
-for(int D=0; D<=C; D++)
-    A=A+D;
-    int factorial = 1;   /*обновлённое значение факториала*/
-    for (int i = 1; i<=A; i++)/*вложенный цикл для факториала*/
-    {
-    /*printf( "i ==============%d\n", i);*/
-    factorial = factorial * i;
-    /*printf( "factorial==============%d\n", factorial);*/
-    }
-    /*printf( "D======%d\n", D );*/
-    SummaFaktorialov = SummaFaktorialov + factorial;
-    /*printf( "SummaFaktorialov===%d\n", SummaFaktorialov );*/
-    }
-    printf( "SummaFaktorialov===%d\n", SummaFaktorialov );
-return 0;
-}
+/*Вычисляем корень этой фукцией*/
+float FunctionKoren (int B, int C)
+{
+Koren = sqrt(B+C);
+printf("Koren===%.2f\n",Koren);
+return Koren;
