bfdce2c0608e769eacb9a5e40b8e861e32fe7669
NazarVolynko <36282087+NazarVolynko@users.noreply.github.com>
2018-03-01 01:29:13 +0200

    Add files via upload

---
+++ dk72/NazarVolynko/LB_1/dk_tool.h
@@ -0,0 +1,7 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+int factorial(int n);
+int functsiaSumy(int chislo_A, int chislo_Q , int (*Yfactorial)(int));
+
+#endif // DK_TOOL_H_INCLUDED
