3aafb37664aa228f3e8aa98eefaba1b516af37a7
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-03-01 12:22:01 +0200

    Create dk_tool.h

---
+++ dk72/PasiutaDmitriy/Lab1/dk_tool.h
@@ -0,0 +1,15 @@
+//
+// Created by dima on 21.02.18.
+//
+
+#ifndef INC_1_LABA_DK_TOOL_H
+#define INC_1_LABA_DK_TOOL_H
+
+int Factorial (int j);
+
+float square(float a);
+
+float Factorialsum(float a);
+
+
+#endif //INC_1_LABA_DK_TOOL_H
