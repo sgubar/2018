d0b03c2a2900e6729e0c7c5906f9c0e131ba88fd
DrCaH4ec <34415950+DrCaH4ec@users.noreply.github.com>
2018-03-01 11:26:09 +0200

    Update dk_tool.c

--- dk72/Shlikhta/HW1/dk_tool.c
+++ dk72/Shlikhta/HW1/dk_tool.c
@@ -2,8 +2,6 @@
 
 #include <string.h>
 
-int num_hex_dec[6], num_dec = 0;
-
 int power(int data_pow, int st)
 {
 	if(st == 0) return 1;
@@ -12,26 +10,17 @@ int power(int data_pow, int st)
 
 int hextodec(char num_hex[10])
 {
-	//comparison A B C D E F with dec "analogue"
-	for(int i = 0; num_hex[i] != '\0' ; i++)
-	{
-		if(num_hex[i] > 47 && num_hex[i] < 58) num_hex_dec[i] = num_hex[i] - 48; 
-		if(num_hex[i] > 64 && num_hex[i] < 71) num_hex_dec[i] = num_hex[i] - 55;
-		if(num_hex[i] > 96 && num_hex[i] < 103) num_hex_dec[i] = num_hex[i] - 87; 
-	}
-	
-	//for(int i = 0; i < strlen(num_hex); i++) printf("%d ", num_hex_dec[i]); //only for test
-	
-	int j = 0;
+	int temp_num, num_dec = 0, str_len = strlen(num_hex);
 	
 	// converting from hex to dec
-	for(int i = strlen(num_hex)-1; i >= 0; i--) 
-	{  
-		num_dec += power(16, i) * num_hex_dec[j]; 
-		j++;
-	} 
-	
-	j = 0;
-	
+	for(int i = 0; num_hex[i] != '\0' ; i++)
+	{
+		if(num_hex[i] >= '0' && num_hex[i] <= '9') temp_num = num_hex[i] - 48; 
+		if(num_hex[i] >= 'A' && num_hex[i] <= 'F') temp_num = num_hex[i] - 55;
+		if(num_hex[i] >= 'a' && num_hex[i] <= 'f') temp_num = num_hex[i] - 87; 
+		
+		num_dec += power(16, str_len-i-1) * temp_num;
+	}
+
 	return num_dec;
 }
