7fd3b1e2063a752536d12d509f5a4791b7c06e62
Никита <36276669+luchikC@users.noreply.github.com>
2018-03-10 22:15:40 +0200

    Add files via upload

---
+++ dk71/Nikita Luchenko/HW/hw1/dk_tool.h
@@ -0,0 +1,11 @@
+#ifndef dk_tool.h
+#define dk_tool.h
+
+#include <stdio.h>
+
+void sumamatrixAB(int verSide, int horSide,	int* matrixAPoiner, int* matrixBPoiner, int* matrixCPoiner);
+void scanmatrix(int verSide, int horSide, int* matrixAPoiner);
+void printmatrix(int verSide, int horSide, int* matrixAPoiner);
+
+#endif 
+
