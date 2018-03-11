0a8d8ff3ede660bc88d5a4e922ac1f075c333a2b
I-N-G-U <36330618+I-N-G-U@users.noreply.github.com>
2018-03-11 16:04:01 +0200

    Update hometask_1.c

--- dk71/DmitryDyachuk/first_lab/hometask_1/hometask_1.c
+++ dk71/DmitryDyachuk/first_lab/hometask_1/hometask_1.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include "hometask_1_func.h"
 
-int ntr_vl_fnc(int strng, int clmn);
-
-void output_matrix(int matrix[], int strngs, int num_comp);
-
 int main()
 {
     int strng_vl = 0;
@@ -54,34 +50,4 @@ int main()
     output_matrix(tr_mtrx, strng_vl, num_of_components);
     printf("\n");
     return 0;
-}
-
-
-int ntr_vl_fnc(int strng, int clmn)
-{
-    int ntr_vl = 0;
-    int vl_rslt = 0;
-            do
-            {
-                printf("\n- Please enter an integer value for component [%i][%i]\n", strng, clmn);
-                printf("[%i][%i] = ");
-                vl_rslt = scanf("%i", &ntr_vl);
-                fflush(stdin);
-                if (vl_rslt == 1)
-                {}else
-                {
-                    printf("\n- Your value does not meet the condition of this programm.\n- Please try again.\n");
-                }
-            }while(vl_rslt != 1);
-        return ntr_vl;
-}
-
-void output_matrix(int matrix[], int strngs, int num_comp)
-{
-    for(int i = 0; i < num_comp; ++i)
-    {
-        if(i != 0 && i % strngs == 0)
-            printf("\n\n\t");
-        printf("%i\t", matrix[i]);
-    }
 }
