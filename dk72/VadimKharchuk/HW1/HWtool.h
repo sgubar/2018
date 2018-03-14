f4069422b77461817ef2736eeb09540d2bf8dd77
M0rel <36273999+M0rel@users.noreply.github.com>
2018-03-14 21:49:59 +0200

    Update HWtool.h

--- dk72/VadimKharchuk/HW1/HWtool.h
+++ dk72/VadimKharchuk/HW1/HWtool.h
@@ -1,15 +1,7 @@
-//
-//H_W_tool.h
-//
-//hw1
-//
-//Created by Vadim Kharhuk on 2/09/18.
-//
-//Copyright © 2018 Vadim Kharchuk. All rights reserved.
 
 #ifndef HWtool_H
 #define HWtool_H
 
-void Seach(char str[]);
+void Search(char *str, char *WhatFind);
 
 #endif	 /* HWtool_h */
