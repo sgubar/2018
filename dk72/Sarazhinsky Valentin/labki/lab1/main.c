286c8d8a3dc6de26131d5acb5203f1ff003851ad
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-03-14 22:05:59 +0200

    Update main.c

--- dk72/Sarazhinsky Valentin/labki/lab1/main.c
+++ dk72/Sarazhinsky Valentin/labki/lab1/main.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "dk_tools.h"
+#include <Math.h>
+int a,b,c;
+int main(int argc, char *argv[]) {
+	printf("enter value A:");
+	scanf("%d",&a);
+	printf("enter value B:");
+	scanf("%d",&b);
+	printf("enter value C:");
+	scanf("%d",&c);
+	float theResult=multiply(a,division(sqrt(b+c),b))+math_sum(c,a);
+	printf("%f \n",theResult);
+	return 0;
+}
