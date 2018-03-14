4e73a1cc2c854deef69541396a6aa2dc74281467
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-03-14 22:07:36 +0200

    Create dk_tools.h

---
+++ dk72/Sarazhinsky Valentin/labki/lab1/dk_tools.h
@@ -0,0 +1,11 @@
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+int factorial(int znach);
+int multiply(int znach1, int znach2);
+int division(int znach1, int znach2);
+int math_sum(int C, int A);
+
+#endif
