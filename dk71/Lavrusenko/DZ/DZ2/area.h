57ac2efb0d173d36e8c676b122a04d5dd99e7cd6
sashalavrus <36297205+sashalavrus@users.noreply.github.com>
2018-03-15 01:56:42 +0200

    Add files via upload

---
+++ dk71/Lavrusenko/DZ/DZ2/area.h
@@ -0,0 +1,28 @@
+#ifndef area_h
+#define area_h
+
+
+
+
+typedef struct point {
+	int x;
+	int y;
+	
+}Point;
+
+
+typedef struct sqere {
+Point *A;
+Point *B;
+Point *C;
+Point *D;
+
+}Sqere;
+
+double area (Sqere *Sqere);
+
+void free_all(Sqere *Sqere);
+
+
+#endif
+
