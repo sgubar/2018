a5cfff970a7a85d7abe7c6d57f8da32908104f2d
Zeroya <36275430+Zeroya@users.noreply.github.com>
2018-03-15 15:20:22 +0200

    Add files via upload

---
+++ dk71/Latyshev/HW1/HM1.h
@@ -0,0 +1,13 @@
+#ifndef _DK_TOOL_H
+#define _DK_TOOL_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int biennial_transformer(void)
+
+#endif	
---
+++ dk71/Latyshev/HW1/hw_main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "dk_tool.h"
+
+
+
+int main(void)
+{
+
+	printf("%i\n", biennial_transformer()); 
+     return 0;
+
+}
+
+
