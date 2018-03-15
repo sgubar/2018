7939f993fc34c75e7124c0bf9e6139586f94d4b5
AlexPays <32728226+alexpays99@users.noreply.github.com>
2018-03-15 13:00:09 +0200

    Add files via upload

---
+++ dk72/Lab_1/HW1/dk_tool.h
@@ -0,0 +1,7 @@
+#ifndef _DK_TOOL_H
+#define _DK_TOOL_H
+#include <stdio.h>
+
+int BinaryToDecimal(void);
+
+#endif	//dk_tool_H
---
+++ dk72/Lab_1/HW1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include "dk_tool.h"
+ 
+int main(void)
+{
+	while(1)
+	{
+		printf("%d\n", BinaryToDecimal());
+	}
+	
+    getch();
+   return 0;
+}
