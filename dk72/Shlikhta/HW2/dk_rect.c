104bf033f4fed16a53996abe59f609f89268dca7
DrCaH4ec <34415950+DrCaH4ec@users.noreply.github.com>
2018-03-17 12:47:07 +0200

    Update dk_rect.c

--- dk72/Shlikhta/HW2/dk_rect.c
+++ dk72/Shlikhta/HW2/dk_rect.c
@@ -1,51 +1,55 @@
-
 #include "dk_rect.h"
 
+void print_coord_point(ppoint pn_rect, int pn_point) 												//print coordinates of figure in format: (x,y)
+{
+	printf("(%.2f,%.2f)  \n", pn_rect->points[pn_point]->x, pn_rect->points[pn_point]->y);
+}
 
-float p_lenght(float p_Ax, float p_Ay, float p_Bx, float p_By)
+void enter_coord_point(ppoint en_rect, int en_point) 												//get coordinates of figure in format: x y
+{
+	scanf("%f %f", &en_rect->points[en_point]->x, &en_rect->points[en_point]->y);
+}
+
+float p_lenght(float p_Ax, float p_Ay, float p_Bx, float p_By) 										//return the length of side of figure
 {
 	return sqrt((p_Bx-p_Ax)*(p_Bx-p_Ax)+(p_By-p_Ay)*(p_By-p_Ay));
 };
 
-ppoint enter_data()
+float ret_point(ppoint r_rect, int rpoint, char rtoc)
+{
+	if(rtoc == 'x') return r_rect->points[rpoint]->x;
+	else return r_rect->points[rpoint]->y;
+}
+
+ppoint enter_data() 																				//get coordinates of figures and check it for right
 {
 	
 	ppoint temp_rect = malloc(sizeof(r_point));
 	
-	temp_rect->points[0] = malloc(sizeof(pnt));
-	temp_rect->points[1] = malloc(sizeof(pnt));
-	temp_rect->points[2] = malloc(sizeof(pnt));
-	temp_rect->points[3] = malloc(sizeof(pnt));
+	for(int i = 0; i < 4;i++) temp_rect->points[i] = malloc(sizeof(pnt));
 	
 	printf("Enter a coordinates of rectangle: \n");
 	
-	printf("A ");
-	scanf("%f %f", &temp_rect->points[0]->x, &temp_rect->points[0]->y);
-	printf("B ");
-	scanf("%f %f", &temp_rect->points[1]->x, &temp_rect->points[1]->y);
-	printf("C ");
-	scanf("%f %f", &temp_rect->points[2]->x, &temp_rect->points[2]->y);
-	printf("D ");
-	scanf("%f %f", &temp_rect->points[3]->x, &temp_rect->points[3]->y);
-	
-	while(p_lenght(temp_rect->points[0]->x, temp_rect->points[0]->y, temp_rect->points[2]->x, temp_rect->points[2]->y) 
-				!= p_lenght(temp_rect->points[1]->x, temp_rect->points[1]->y, temp_rect->points[3]->x, temp_rect->points[3]->y))
+	for(int i = 0; i < 4; i++)
 	{
-		printf("");
-		printf("ERR((( ->Invalid coordinates<-\nEnter a coordinates of rectangle: \n");
-	
-	printf("A ");
-	scanf("%f %f", &temp_rect->points[0]->x, &temp_rect->points[0]->y);
-	printf("B ");
-	scanf("%f %f", &temp_rect->points[1]->x, &temp_rect->points[1]->y);
-	printf("C ");
-	scanf("%f %f", &temp_rect->points[2]->x, &temp_rect->points[2]->y);
-	printf("D ");
-	scanf("%f %f", &temp_rect->points[3]->x, &temp_rect->points[3]->y);
-		
+		printf("%dth ", i);
+		enter_coord_point(temp_rect, i);
 	}
 	
-	printf("The rectangle is right))\n");
+																									//checking for a right entering temp_rect->points[0]->x
+	while(p_lenght(ret_point(temp_rect,0,'x'), ret_point(temp_rect,0,'y'), ret_point(temp_rect,2,'x'), ret_point(temp_rect,2,'y')) 
+				!= p_lenght(ret_point(temp_rect,1,'x'), ret_point(temp_rect,1,'y'), ret_point(temp_rect,3,'x'), ret_point(temp_rect,3,'y')))
+	{
+		printf("ERR((( ->Invalid coordinates<-\nEnter a coordinates of rectangle: \n");
+	
+		for(int i = 0; i < 4; i++)
+		{
+			printf("%dth ", i);
+			enter_coord_point(temp_rect, i);
+		}	
+	}
+	
+	printf("\nThe rectangle is right))\n\n");
 	
 	return temp_rect;
 };
@@ -55,23 +59,24 @@ void print_param(ppoint par_rect)
 {
 	float AB, BC;
 	
-	AB = p_lenght(par_rect->points[0]->x, par_rect->points[0]->y, par_rect->points[1]->x, par_rect->points[1]->y);
-	BC = p_lenght(par_rect->points[1]->x, par_rect->points[1]->y, par_rect->points[2]->x, par_rect->points[2]->y);
+	AB = p_lenght(ret_point(par_rect,0,'x'), ret_point(par_rect,0,'y'), ret_point(par_rect,1,'x'), ret_point(par_rect,1,'y'));
+	BC = p_lenght(ret_point(par_rect,1,'x'), ret_point(par_rect,1,'y'), ret_point(par_rect,2,'x'), ret_point(par_rect,2,'y'));
 	
-	printf("Parameters of rectangle:\n\n");
+	printf("Parameters of rectangle:\n");
 	
 	printf("    ****Coordinates****\n");
-	printf("A = (%.2f,%.2f)  ", par_rect->points[0]->x, par_rect->points[0]->y);
-	printf("B = (%.2f,%.2f)\n", par_rect->points[1]->x, par_rect->points[1]->y);
-	printf("C = (%.2f,%.2f)  ", par_rect->points[2]->x, par_rect->points[2]->y);
-	printf("D = (%.2f,%.2f)\n\n", par_rect->points[3]->x, par_rect->points[3]->y);
 	
-	printf("   ****Length of sides****\n");
-	printf("AB = CD = %.2f ", AB);
-	printf("BC = AD =%.2f\n\n", BC);
+	for(int i = 0; i < 4; i++) 
+	{
+		printf("%dth = ", i);
+		print_coord_point(par_rect, i);
+	}
+		
+	printf("\n");
+
+	printf("   ****Length of sides****\nAB = CD = %.2f\nBC = AD = %.2f\n\n", AB, BC);
 	
-	printf("   ****Square****\n");
-	printf("S = %.2f sq. un.\n", AB*BC);
+	printf("   ****Square****\nS = %.2f sq. un.\n", AB*BC);
 	
 };
 
@@ -81,8 +86,9 @@ void deleting(ppoint del_rect)
 	for(int i = 0; i < 4; ++i) free(del_rect->points[i]); 
 	
 	free(del_rect);
-	printf("Deleting is succesfull!))\n");
-	
+	printf("\nDeleting is succesfull!))\n");
 }
+
+
 
 
