bd8933da237709f866ce316566d692f5f7f4e476
John <33196127+WIllsnSmith@users.noreply.github.com>
2018-03-21 05:33:09 +0200

    Update main.c

--- dk72/winston/HM1/main.c
+++ dk72/winston/HM1/main.c
@@ -7,8 +7,10 @@
 
 int main ()
 {
+        int *c;
+        int *b;
         int *a;
-        int i, j, n, m;
+        int i, j, n, m, l, o, h, t;
 
 
         printf("Enter arrows: \n");
@@ -18,5 +20,16 @@ int main ()
 
 
         yourmatr1(a, m, n);
-}
 
+        printf("Enter arrows: \n");
+        scanf("%d", &l);
+        printf("Enter columns: \n");
+        scanf("%d", &o);
+
+
+        yourmatr2(b, l, o);
+
+        printf("\n\n    The result of subtraction matrix:\n");
+
+        yourmatr3(c, h, t);
+}
