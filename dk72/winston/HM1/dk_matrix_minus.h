65f0455f7bd5f3c7c50c0f938b8db09c52d5fa9c
John <33196127+WIllsnSmith@users.noreply.github.com>
2018-03-21 02:15:17 +0200

    Update dk_matrix_minus.h

--- dk72/winston/HM1/dk_matrix_minus.h
+++ dk72/winston/HM1/dk_matrix_minus.h
@@ -1,16 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
 #include <stdlib.h>
 
-void entermatr1 (int a);
-
-void printmatr1 (int a);
-
-void entermatr2 (int a);
-
-void printmatr2 (int a);
-
-int minusmatr (int a);
-
-void printmatr3 (int a);
+    int** yourmatr1 (int a);
