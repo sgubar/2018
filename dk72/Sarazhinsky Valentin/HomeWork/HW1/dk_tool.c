34af5659a99e511ba91e4f0d346e540dad6baeec
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-03-22 23:19:32 +0200

    Update dk_tool.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW1/dk_tool.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW1/dk_tool.c
@@ -1,21 +1,40 @@
 #include "dk_tool.h"
-int i,j;
-int transponation(int lines, int columns)
-{
 
-	int matrix[lines][columns];
+int i, j, lines, columns;
+int matrix[][100],trans_matrix[][100];
+
+int input_matrix()
+{
+	printf("enter the length of the lines:");
+	scanf("%d",&lines);
+	printf("enter the length of the columns:");
+	scanf("%d",&columns);
+
 	for(i=0;i<lines;i++){
 		for(j=0;j<columns;j++){
 			printf("matrix[%d][%d]:",i,j);
 			scanf("%d",&matrix[i][j]);		
 		}
 	}
-		for(i=0;i<columns;i++){
+	
+}
+
+int transponation()
+{
+	for(i=0;i<lines;i++){
+		for(j=0;j<columns;j++){
+			trans_matrix[j][i]=matrix[i][j];			
+		}
+	}
+}
+
+int output_matrix()
+{
+	for(i=0;i<columns;i++){
 		for(j=0;j<lines;j++){
-			printf("%d    ",matrix[j][i]);			
+			printf("%d    ",trans_matrix[i][j]);			
 		}
 		printf("\n");
 	}
 	
-	return 0;
 }
