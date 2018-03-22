1f0132c74cb03443664d5a59039d8730cea1515a
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-03-22 23:16:56 +0200

    Update main.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dk_tool.h"
- int lines,columns;
+
  
 int main(int argc, char *argv[]){
-	printf("enter the length of the lines:");
-	scanf("%d",&lines);
-	printf("enter the length of the columns:");
-	scanf("%d",&columns);
-	transponation(lines, columns);
-	
+ 
+	input_matrix();
+	transponation();
+	output_matrix();
 	return 0;
 }
