6f0793f1de555533a298c9f1401531c2e24502e6
DrCaH4ec <34415950+DrCaH4ec@users.noreply.github.com>
2018-03-23 14:42:24 +0200

    Update main.c

--- dk72/Shlikhta/LAB2/main.c
+++ dk72/Shlikhta/LAB2/main.c
@@ -1,10 +1,12 @@
 #include "l2.h"
 
+
 int main(void)
 {
 	int ar_size = enter_arr_size(); // entering a size of future array
 	
-	int array[ar_size]; //creating an array
+	
+	int *array = malloc(sizeof(int)*ar_size); //creating an array
 	
 	ent_data_to_arr(array, ar_size); //entering data to array
 	write_to_file(array, ar_size, "bubble_b"); // writing to file an array before sorting
@@ -28,7 +30,6 @@ int main(void)
 	chousen_sort(array, ar_size); // sorting an array
 		printf("Time of chousen sorting is %.4lf sec\n", (clock() - start) / CLOCKS_PER_SEC); //printing a time of making sorting
 	write_to_file(array, ar_size, "chousen_a"); // writing to file an array after sorting
-		
 	
 	return 0;
 }
