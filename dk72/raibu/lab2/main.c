316d9e4140a8ac9ffb022acb730ae65ad499e949
RabuRaibu <k561la3@meta.ua>
2018-03-23 01:36:11 +0000

    proper output, no buggy merge sort

--- dk72/raibu/lab2/main.c
+++ dk72/raibu/lab2/main.c
@@ -11,51 +11,53 @@
 
 int main(int argc, char const *argv[])
 {
-	// int LEN = atoi(argv[1]);
-	// struct rusage usage;
-	// struct timeval start, end;
+	if(argc != 3){
+		printf("usage : ./main <num> <file>\n");
+	}
+	FILE* fp = fopen(argv[2], "w");
 
-	// int* a = gen_rand(LEN);
+	int LEN = atoi(argv[1]);
+	struct rusage usage;
+	struct timeval start, end;
 
-	// getrusage(RUSAGE_SELF, &usage);
-	// start = usage.ru_utime;
+	int* a = gen_rand(LEN);
 
-	// bsort(a, LEN);
+	getrusage(RUSAGE_SELF, &usage);
+	start = usage.ru_utime;
 
-	// getrusage(RUSAGE_SELF, &usage);
- //  	end = usage.ru_utime;
- //  	printf("array of length %d buble sorted in %ld.%0.6ld s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
- //  	free(a);
+	bsort(a, LEN);
 
- //  	a = gen_rand(LEN);
+	getrusage(RUSAGE_SELF, &usage);
+  	end = usage.ru_utime;
+  	fprintf(fp,"array of length %d buble sorted in %lu.%0.6lu s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
+  	free(a);
 
- //  	getrusage(RUSAGE_SELF, &usage);
-	// start = usage.ru_utime;
+  	a = gen_rand(LEN);
 
-	// isort(a, LEN);
+  	getrusage(RUSAGE_SELF, &usage);
+	start = usage.ru_utime;
+
+	isort(a, LEN);
 	
-	// getrusage(RUSAGE_SELF, &usage);
- //  	end = usage.ru_utime;
- //  	printf("array of length %d insertion sorted in %ld.%0.6ld s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
- //  	free(a);
+	getrusage(RUSAGE_SELF, &usage);
+  	end = usage.ru_utime;
+  	fprintf(fp,"array of length %d insertion sorted in %lu.%0.6lu s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
+  	free(a);
 
- //  	a = gen_rand(LEN);
+  	a = gen_rand(LEN);
 
- //  	getrusage(RUSAGE_SELF, &usage);
-	// start = usage.ru_utime;
+  	getrusage(RUSAGE_SELF, &usage);
+	start = usage.ru_utime;
 
-	// csort(a, LEN);
+	csort(a, LEN);
 	
-	// getrusage(RUSAGE_SELF, &usage);
- //  	end = usage.ru_utime;
- //  	printf("array of length %d choice sorted in %ld.%0.6ld s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
+	getrusage(RUSAGE_SELF, &usage);
+  	end = usage.ru_utime;
+  	fprintf(fp,"array of length %d choice sorted in %lu.%0.6lu s\n",LEN, end.tv_sec-start.tv_sec, end.tv_usec - start.tv_usec);
 
- //  	free(a);
+  	free(a);
 
-
-int a[5] = {1,32768, 123456, 7654321, 1000};
-msort(a,5); 
-print_arr(a,5);
+  	
 
 
 
