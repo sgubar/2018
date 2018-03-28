0fae0edd6ff089588d2b6a0e3f2c50a547e09dc4
syabrenko <36296755+syabrenko@users.noreply.github.com>
2018-03-28 23:00:41 +0300

    Create main.c

---
+++ dk72/AlexandraSyabrenko/Lab1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "functions.h"
+
+
+int main() {
+    int B;
+    float A, C, Q;
+    
+    printf("Hello, I am a calculator that knows only one formula ((Aˆ2+Bˆ2)/(B+5ˆC))+B!\n");
+    printf("Input A: ");
+    scanf("%f", &A);
+    printf("Input B: ");
+    scanf("%d", &B);
+    printf("Input C: ");
+    scanf("%f", &C);
+    
+    while (B < 0)
+    {
+        printf("The argument for the factorial can not be less than zero. Please, input another value: ");
+        scanf("%d", &B);
+    }
+    
+   
+    Q = (power(A,2)+power(B,2))/(B + power(5, C))+factorial(B);
+    printf("Result = %.4f\n", Q);
+    
+    
+    return 0;
+}
+
+
