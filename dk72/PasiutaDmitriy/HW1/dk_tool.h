e5495dccbc5bf7468a2355479fcb514a1537e2a8
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-04-05 00:03:54 +0300

    Create dk_tool.h

---
+++ dk72/PasiutaDmitriy/HW1/dk_tool.h
@@ -0,0 +1,6 @@
+#ifndef HW1_DK_TOOL_H
+#define HW1_DK_TOOL_H
+#include <stdio.h>
+
+void additionmatrix(int length, int resultmatrix[length][length], int firstmatrix[length][length], int secondmatrix[length][length]);
+#endif //HW1_DK_TOOL_H
