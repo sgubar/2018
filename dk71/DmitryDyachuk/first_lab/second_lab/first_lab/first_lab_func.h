1ff5fe36c26a1d723b9018412a0fb1380287dd01
I-N-G-U <36330618+I-N-G-U@users.noreply.github.com>
2018-04-05 10:26:45 +0300

    Add files via upload

---
+++ dk71/DmitryDyachuk/first_lab/second_lab/first_lab/first_lab_func.h
@@ -0,0 +1,15 @@
+//
+//first_lab_func.h
+//
+//lab1
+//
+//Created by Dmitry Dyachuk on 2/09/18.
+//
+//Copyright © 2018 Dmitry Dyachuk. All rights reserved.
+
+int factorial(int factorial_value); // function for calculating output of factorial.
+int numerator(int numerator_A, int numerator_B); // function for calculating output of numerator.
+int second_degree(int value_degree); // function for calculation output of the second power.
+int C_degree(int C_value); // function for calculating output the C-th power.
+int denominator(int denominator_B, int denominator_C); // function for calculating output of denominator.
+float all_expression(int factorial_result, int numerator_result, float denominator_resut); // function for calculating output of all expression.
