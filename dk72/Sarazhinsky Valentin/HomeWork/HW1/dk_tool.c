fb620d1181b49737e2d82947fbf097751a91ae47
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-04-12 00:34:47 +0300

    Update dk_tool.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW1/dk_tool.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW1/dk_tool.c
@@ -1,38 +1,36 @@
 #include "dk_tool.h"
-
-int i, j, lines, columns;
-int matrix[][100],trans_matrix[][100];
-
-int input_matrix()
+int i, j;
+int input_matrix(int *matrix, int lines, int columns)
 {
-	printf("enter the length of the lines:");
-	scanf("%d",&lines);
-	printf("enter the length of the columns:");
-	scanf("%d",&columns);
+	
 
 	for(i=0;i<lines;i++){
 		for(j=0;j<columns;j++){
-			printf("matrix[%d][%d]:",i,j);
-			scanf("%d",&matrix[i][j]);		
+			printf("matrix[%d][%d]:",i+1,j+1);
+			scanf("%d",&matrix[(i*lines) + j]);		
 		}
 	}
+	return matrix;
 	
 }
 
-int transponation()
+
+int transponation(int *matrix, int *trans_matrix, int lines, int columns)
 {
 	for(i=0;i<lines;i++){
 		for(j=0;j<columns;j++){
-			trans_matrix[j][i]=matrix[i][j];			
+			trans_matrix[(j*columns) + i] = matrix[(i*lines) + j];			
 		}
 	}
+	return trans_matrix;
+	
 }
 
-int output_matrix()
+int output_matrix(int *trans_matrix, int lines, int columns)
 {
-	for(i=0;i<columns;i++){
-		for(j=0;j<lines;j++){
-			printf("%d    ",trans_matrix[i][j]);			
+		for(i=0;j<columns;j++){
+		for(j=0;i<lines;i++){
+		printf("%d    ",trans_matrix[(j*columns) + i]);			
 		}
 		printf("\n");
 	}
