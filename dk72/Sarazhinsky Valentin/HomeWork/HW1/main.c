7158baab03d7bf82aa51fb6c1c4086e153cbba31
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-04-12 00:34:00 +0300

    Update main.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
@@ -5,8 +5,20 @@
  
 int main(int argc, char *argv[]){
  
-	input_matrix();
-	transponation();
-	output_matrix();
+ 	int *matrix, *trans_matrix, lines, columns;
+ 	
+	printf("enter the length of the lines:");
+	scanf("%d",&lines);
+	printf("enter the length of the columns:");
+	scanf("%d",&columns);
+	
+	matrix = (int*)malloc((lines+columns)*sizeof(int));  
+    trans_matrix = (int*)malloc((columns+lines)*sizeof(int));
+	
+	matrix = input_matrix(matrix, lines, columns);
+	trans_matrix = transponation(matrix, trans_matrix, lines, columns);
+	output_matrix(trans_matrix, lines, columns);
+	free(matrix);
+	free(trans_matrix);
 	return 0;
 }
