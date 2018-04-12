9ce642c8704b8b92331b45abadd3cd24f57a5221
ant112342 <36268347+ant112342@users.noreply.github.com>
2018-04-12 08:43:38 +0300

    Create main.c

---
+++ dk72/ant112342/LABA2/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include <conio.h>
+#include<time.h>
+#include"dk_sort.h"
+
+int main()
+{
+   FILE*file;
+   int m=5043,n=32;
+   char *words[m][n];
+read_it_from(file,words);
+}
