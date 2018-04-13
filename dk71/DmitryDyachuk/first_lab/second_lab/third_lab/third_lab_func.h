6c25ce14778f2544cbfc1a45c7f84b8ab9150733
I-N-G-U <36330618+I-N-G-U@users.noreply.github.com>
2018-04-13 11:22:20 +0300

    Delete third_lab_func.h

--- dk71/DmitryDyachuk/first_lab/second_lab/third_lab/third_lab_func.h
+++
@@ -1,5 +0,0 @@
-int first_write_to_file_func(int array_length);
-char * scan_from_file_func(int array_length);
-void quick_sort_func(char * letters, int first, int last);
-void line_search(char * letters,int array_length);
-int second_write_to_file_func(char * letters, int array_length);
