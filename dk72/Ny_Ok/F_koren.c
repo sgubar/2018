04b569ba4a6cb8f030f838bed44e5451140cc6d8
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-04-17 15:34:08 +0300

    Delete F_koren.c

--- dk72/Ny_Ok/F_koren.c
+++
@@ -1,9 +0,0 @@
-#include "FileZagolovok.h"
-float Koren;
-/*Вычисляем корень этой фукцией*/
-float FunctionKoren (int B, int C)
-{
-Koren = sqrt(B+C);
-printf("Koren===%.2f\n",Koren);
-return Koren;
-}
