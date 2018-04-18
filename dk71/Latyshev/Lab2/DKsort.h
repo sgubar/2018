1bc811f90b800f5bf407b17a5a7dde6a4a152b7f
Zeroya <36275430+Zeroya@users.noreply.github.com>
2018-04-18 21:54:02 +0300

    Add files via upload

---
+++ dk71/Latyshev/Lab2/DKsort.h
@@ -0,0 +1,29 @@
+#ifndef _DKSORT_H
+
+#define _DKSORT_H
+
+
+
+#include <stdio.h>
+
+#include <time.h>
+
+
+
+void swap (int *ELx, int *ELy);
+
+void three_type_sort( int array[], int size, int selection);
+
+void S_Sort(int array[], int size);
+
+void B_Sort(int array[], int size);
+
+void I_Sort(int array[], int size);
+
+void random_letter(int array[], int size);
+
+void Rec_To_File(int array[], int size, char* name);
+
+
+
+#endif 
