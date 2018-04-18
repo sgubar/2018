6177f2c0bfea565975c8300d2ea39474fb4f2123
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-04-18 23:36:11 +0300

    Update main.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW3/main.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW3/main.c
@@ -1,54 +1,48 @@
-#include <stdio.h>
 #include "dk_square.h"
 #include "dk_square_array.h"
-#include "Math.h"
+
 
 int main(int argc, const char * argv[])
-
 {
-	
-	
-	int xA, yA, xB, yB, xC, yC, xD, yD;
-	printf("enter the coordinates of point A(x,y):");
-	scanf("%d,%d",&xA,&yA);
-	printf("enter the coordinates of point B(x,y):");
-	scanf("%d,%d",&xB,&yB);
-	printf("enter the coordinates of point C(x,y):");
-	scanf("%d,%d",&xC,&yC);
-	while(pow(xA-xB,2)+pow(yA-yB,2)+pow(xC-xB,2)+pow(yC-yB,2)!=pow(xA-xC,2)+pow(yA-yC,2) ||
-            pow(xA-xB,2)+pow(yA-yB,2)!=pow(xC-xB,2)+pow(yC-yB,2))
-    {
-      	printf("incorrectly entered data !! try again ..\n") ; 
-		printf("enter the coordinates of point C(x,y):");
-		scanf("%d,%d",&xC,&yC);    	
-	}
-	xD=(xA+xC)-xB;
-    yD=(yA+yC)-yB;
-    
 
-	SquareArray *theArray = createArray(10);
-	
-    
-	Point A = {xA,yA};
-	Point B = {xB,yB};
-	Point C = {xC,yC};
-	Point D = {xD,yD};
-	
-	Square *ABCD = createSquare(&A, &B, &C, &D);
+    SquareArray *theArray = createArray(5);
+
+ int A_x, A_y, B_x, B_y, C_x, C_y;
+ int count=1, N=1;
+
+do
+ {
+   
+    printf("enter the coordinates of the point A:\n");
+    input_coordinates (&A_x,&A_y);
+    printf("enter the coordinates of the point B:\n");
+    input_coordinates (&B_x,&B_y);
+	printf("enter the coordinates of the point C:\n");
+	input_coordinates_C (&C_x,&C_y, A_x, A_y, B_x, B_y);
+
+	Point A = {A_x,A_y};
+	Point B = {B_x,B_y};
+	Point C = {C_x,C_y};
+	Point D = {(A_x+C_x)-B_x,(A_y+C_y)-B_y};
+
+	Square ABCD={&A, &B, &C, &D};
+
 	addElement(theArray, &ABCD);
-	
-	
-	FILE *theFile = fopen("Users\valentin\Desktop\test.json", "a+");
-	
+	printf("input next square - 1; finish -0?\n");
+	scanf("%d",&count);
+	K++;
+ }while(count==1 && N<=5);
+
+	FILE *theFile = fopen("C:/test.json", "a+");
+
 	writeArrayToJSON(theFile, theArray);
-	
+
 	fflush(theFile);
 	fclose(theFile);
-	
-	freeArray(theArray);
-	// insert code here...
-	printf("code finished work...\n");
 
-	destroySquare(ABCD);
+	freeArray(theArray);
+
+	printf("\n code finished work...\n");
+
 	return 0;
 }
