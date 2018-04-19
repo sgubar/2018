4f3838d1a3402abfb50d60c36ff82ee47f8d713d
ant112342 <36268347+ant112342@users.noreply.github.com>
2018-04-19 12:42:16 +0300

    Add files via upload

---
+++ dk72/ant112342/HW4/dk_rectangle_array.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include "dk_rectangle_array.h"
+
+RectangleArray *createArray(int aNumber)
+{
+	RectangleArray *theResult = NULL;
+
+	if (aNumber > 0)
+	{
+		theResult = (RectangleArray *)malloc(sizeof(RectangleArray));
+
+		if (NULL != theResult)
+		{
+			theResult->rectangles = (Point **)malloc(sizeof(Point *)*aNumber);
+            theResult->Ploscha = malloc(sizeof(float)*aNumber);
+			if (NULL != theResult->rectangles)
+			{
+				theResult->number = aNumber;
+				theResult->count = 0;
+			}
+			else
+			{
+				free(theResult);
+				theResult = NULL;
+			}
+		}
+	}
+
+	return theResult;
+}
+
+void freeArray(RectangleArray *anArray)
+{
+	if (NULL != anArray)
+	{
+		for (int i = 0; i < anArray->count; i ++)
+		{
+			destroyRectangle(anArray->rectangles[i]);
+		}
+
+		free(anArray->rectangles);
+		free(anArray);
+	}
+}
+
+int addElement(RectangleArray *anArray, Point *aRectangle,float Ploscha)
+{
+	int theResult = -1;
+
+	if (NULL != anArray && NULL != aRectangle &&
+				anArray->count < anArray->number)
+	{
+		anArray->rectangles[anArray->count] =
+					createRectangle(aRectangle->A, aRectangle->B,aRectangle->C,aRectangle->D);
+
+					anArray->Ploscha[anArray->count] = Ploscha;
+		theResult = anArray->count;
+		anArray->count ++;
+	}
+
+	return theResult;
+}
+
+void writeArrayToJSON(FILE *aFile, RectangleArray *anArray)
+{
+	if (NULL == anArray || NULL == aFile)
+	{
+		return ;
+	}
+
+	fprintf(aFile, "{\n\"number\" : %d,\n\"count\" : %d,\n",
+			anArray->number, anArray->count);
+	fprintf(aFile, "\"rectangle\" : \n[\n");
+
+	for (int i = 0; i < anArray->count; i ++)
+	{
+		writeRectangleToJSON(aFile, anArray->rectangles[i]);
+
+		if (i < anArray->count - 1)
+		{
+			fprintf(aFile, ",");
+		}
+		fprintf(aFile, "\n");
+	}
+
+	fprintf(aFile, "],\n}");
+}
+void inception_sort(RectangleArray *anArray)
+{
+    Point *newElement=NULL;
+    int  i,j;
+    for (j = 1; j <anArray->count ; j++) {
+        newElement = anArray->rectangles[j];
+        for (i = j;anArray->Ploscha[i-1] > anArray->Ploscha[i] && i > 0; i--) {
+            anArray->rectangles[i] = anArray->rectangles[i-1];
+        }
+    anArray->rectangles[i] = newElement;
+
+    }
+
+
+}
+
+
---
+++ dk72/ant112342/HW4/dk_rectangle_array.h
@@ -0,0 +1,24 @@
+#ifndef dk_rectangle_array_h
+#define dk_rectangle_array_h
+
+#include <stdio.h>
+#include "dk_rectangle.h"
+typedef struct tagRectangleArray
+{
+	int number;
+	int count;
+
+	Point **rectangles;
+	float *Ploscha;
+}RectangleArray;
+
+RectangleArray *createArray(int aNumber);
+void freeArray(RectangleArray *anArray);
+int addElement(RectangleArray *anArray, Point *aLine,float Ploscha);
+void writeArrayToJSON(FILE *aFile, RectangleArray *anArray);
+void inception_sort(RectangleArray *anArray);
+
+#endif
+
+
+
