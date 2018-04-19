b2ce07a3336b94790469ef5ef77c4c8b6854e525
di-raiy <36276100+di-raiy@users.noreply.github.com>
2018-04-19 15:18:29 +0300

    Add files via upload

---
+++ dk71/di-raiy/laba3/lab3.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getfile(char mainfile[], char str[] );
+void sortirovkaShella(char str[], int count);
+int Linesearch(char str[], char znach, int count);
