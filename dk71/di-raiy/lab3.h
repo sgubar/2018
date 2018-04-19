f7ef4e380c9020088bbb5db19f4468c6d5bd8eb2
di-raiy <36276100+di-raiy@users.noreply.github.com>
2018-04-19 15:17:57 +0300

    Delete lab3.h

--- dk71/di-raiy/lab3.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int getfile(char mainfile[], char str[] );
-void sortirovkaShella(char str[], int count);
-int Linesearch(char str[], char znach, int count);
