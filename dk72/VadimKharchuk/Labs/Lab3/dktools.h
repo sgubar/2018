b0fb372abfb3ba2e69c0df475e1cbb35bdd7cc54
M0rel <36273999+M0rel@users.noreply.github.com>
2018-04-20 11:36:12 +0300

    Create dktools.h

---
+++ dk72/VadimKharchuk/Labs/Lab3/dktools.h
@@ -0,0 +1,17 @@
+//
+// Created by vadim on 20.04.2018.
+//
+
+#ifndef LAB3_DKTOOLS_H
+#define LAB3_DKTOOLS_H
+
+#include <stdio.h>
+#include <time.h>
+
+
+void Random(int * ,long );
+long ValueCheck();
+void quickSort(int [], int, int );
+void printArrToFile(int * , long , char* );
+void Seach(int *, int , long);
+#endif //LAB3_DKTOOLS_H
