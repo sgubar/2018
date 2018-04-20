ad280fc4068429d8ac9c61726ea246532bd3be12
LoveFELAnoniy <36298304+LoveFELAnoniy@users.noreply.github.com>
2018-04-20 13:06:08 +0300

    Add files via upload

---
+++ dk72/Anton Voloshyn/LAB1/HW2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dk_tool.h"
+
+int main()
+{
+	Point A = {4,9};
+	Point B = {-2,5};
+	Point C = {-2,9};
+
+    Triangle *ABC = createTriangle(&A, &B, &C);
+
+    if(ABC == NULL)
+    {
+        printf("oop, error\n");
+    }
+    else
+    {
+        printf("Square = %.2f\n", (float)AreaOfTriangle(ABC));
+    }
+
+    destroyTriangle(ABC);
+}
