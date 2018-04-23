fdfd1f6be0b18f14a60a16f79bdc2433689bcda3
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-04-23 22:51:29 +0300

    Create Sort_V10.h

---
+++ dk72/Ny_Ok/Laba 2/Sort_V10.h
@@ -0,0 +1,15 @@
+#ifndef SORT_V10_H_INCLUDED
+#define SORT_V10_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+#include <time.h>
+int n_word();
+int sort_pus();
+int sort_vybr();
+int sort_vctavka();
+
+#endif // SORT_V10_H_INCLUDED
