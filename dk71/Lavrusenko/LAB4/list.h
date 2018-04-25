c247660d8026e599643ae78b24cb1d9ebb07b31d
sashalavrus <36297205+sashalavrus@users.noreply.github.com>
2018-04-26 00:22:38 +0300

    Add files via upload

---
+++ dk71/Lavrusenko/LAB4/list.h
@@ -0,0 +1,30 @@
+#ifndef list_h
+#define list_h
+
+#include <stdio.h>
+
+
+typedef struct Item_of_list Item_List;
+
+typedef struct __ListOptions
+{
+	Item_List *head;
+	Item_List *tail;
+	int count;
+}ListOptions;
+
+extern const int kSLListError;
+
+//Interface
+//Create/delete a list
+ListOptions *SLCreateList();
+void SLFreeList(ListOptions *aList);
+
+Item_List *SLAddNode(ListOptions *aList, Item_List *aNewNode);
+
+int SLCountList(const ListOptions *aList);
+Item_List *SLNodeAtIndex(const ListOptions *aList, int aIndex);
+
+
+
+#endif 
