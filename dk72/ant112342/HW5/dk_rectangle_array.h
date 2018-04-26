46465daba13f48118dc189d0140f626132149a09
ant112342 <36268347+ant112342@users.noreply.github.com>
2018-04-26 13:50:39 +0300

    Add files via upload

---
+++ dk72/ant112342/HW5/dk_rectangle_array.h
@@ -0,0 +1,25 @@
+#ifndef dk_rectangle_array_h
+#define dk_rectangle_array_h
+
+#include <stdio.h>
+#include "dk_rectangle.h"
+typedef struct tagRectangleArray
+{
+	int number;
+	int count;
+
+	Point **rectangles;
+	float *Ploscha;
+}RectangleArray;
+
+RectangleArray *createArray(int aNumber);
+void freeArray(RectangleArray *anArray);
+int addElement(RectangleArray *anArray, Rectangle *aLine,float Ploscha);
+void writeArrayToJSON(FILE *aFile, RectangleArray *anArray);
+int linear_search(RectangleArray *anArray, float key);
+void shell_sort(RectangleArray *anArray);
+
+#endif
+
+
+
