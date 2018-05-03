5d4b4be01555b05b6adc4bec477a31e9fcfb25e8
Zeroya <36275430+Zeroya@users.noreply.github.com>
2018-05-03 21:45:23 +0300

    Update circle_array.h

--- dk71/Latyshev/HW4/circle_array.h
+++ dk71/Latyshev/HW4/circle_array.h
@@ -21,7 +21,7 @@ CircleArray *CreateArray(int aNumber);
 
 void freeArray(CircleArray *InstantArray);
 
-void addElement(CircleArray *InstantArray, Circle *aCircle, float area);
+void addElement(CircleArray *InstantArray, Circle *aCircle);
 
 void insertion_sort(CircleArray *InstantArray);
 
