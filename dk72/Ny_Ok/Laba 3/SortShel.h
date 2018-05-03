9a9cf4543b126803bba40c470587cc3df97b4af1
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-05-03 11:22:55 +0300

    Create SortShel.h

---
+++ dk72/Ny_Ok/Laba 3/SortShel.h
@@ -0,0 +1,15 @@
+#ifndef SORTSHEL_H_INCLUDED
+#define SORTSHEL_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+#include <time.h>
+int n_word();
+int sort_vctavka();
+void sort_Shell();
+char find2();
+
+#endif // SORTSHEL_H_INCLUDED
