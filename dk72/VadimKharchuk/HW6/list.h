82a84e2092e4a2bb38ef35be0e18e8f3fd22599b
M0rel <36273999+M0rel@users.noreply.github.com>
2018-05-04 14:37:53 +0300

    Create list.h

---
+++ dk72/VadimKharchuk/HW6/list.h
@@ -0,0 +1,33 @@
+//
+// Created by vadim on 03.05.2018.
+//
+
+#ifndef HW6_LIST_H
+#define HW6_LIST_H
+typedef struct __tagIntNode IntNode;
+struct __tagIntNode
+{
+    int value;
+    IntNode *prev;
+    IntNode *next;
+};
+
+typedef struct __tagIntList
+{
+    IntNode *head;
+    IntNode *tail;
+    int count;
+
+}IntList;
+
+
+IntList *CreateIntList();
+IntNode *createIntNodeWithValue (int );
+void DestroyIntList(IntList *aList);
+int AddIntValueToList (IntList *aList, int );
+IntNode *createIntNodeWithValue(int );
+void PrintList (IntList *aList);
+int InsertIntValueAtIndex(IntList *aList, int anIntValue, int anIndex);
+void quickSort(IntList *aList, int left, int right);
+
+#endif //HW6_LIST_H
