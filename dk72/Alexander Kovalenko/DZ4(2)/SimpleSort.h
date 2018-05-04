f9fa37e0bf2dbfe52e331f1a922b4620f106b283
sankodk72 <36281112+sankodk72@users.noreply.github.com>
2018-05-04 14:05:37 +0300

    Add files via upload

---
+++ dk72/Alexander Kovalenko/DZ4(2)/SimpleSort.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSort.h
+//  demoSimpleSort
+//
+//  Created by Slava Gubar on 4/7/16.
+//  Copyright © 2016 Slava Gubar. All rights reserved.
+//
+
+#ifndef SimpleSort_h
+#define SimpleSort_h
+
+#include <stdio.h>
+#include "dk_ellipse_array.h"
+
+void bubbleSort(int anArray[], int aSize);
+void selectionSort(int anArray[], int aSize);
+void insertionSort(int anArray[], int aSize);
+
+#endif /* SimpleSort_h */
