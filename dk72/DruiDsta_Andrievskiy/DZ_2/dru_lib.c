b67caf5bfb2234b7579254a3f089fb2ed712a473
DruiDsta <33695583+DruiDsta@users.noreply.github.com>
2018-05-05 22:36:14 +0300

    Add files via upload

---
+++ dk72/DruiDsta_Andrievskiy/DZ_2/dru_lib.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "dru_lib.h"
+
+Fig *createFig(Point *A, Point *B, Point *C)
+{
+	Fig *result=NULL;
+	if(NULL!=A && NULL!=B && NULL!=C)
+	{
+		result=(Fig *)malloc(sizeof(Fig));
+		if(NULL!=result)
+		{
+			result->A=A;
+			result->B=B;
+			result->C=C;
+		}
+	}
+	return result;
+}
+
+void destroyFig(Fig *aFig)
+{
+	if (NULL != aFig)
+	{
+		free(aFig->A);
+		free(aFig->B);
+		free(aFig->C);
+		
+		free(aFig);
+	}
+}
+
+Param *enterParam(Fig *aFig)
+{
+	Param *result=NULL;
+	if(NULL!=aFig->A && NULL!=aFig->B && NULL!=aFig->C)
+	{
+		result=(Param *)malloc(sizeof(Param));
+		
+		float temp_aX=aFig->C->x - aFig->A->x, 
+		temp_aY=aFig->C->y - aFig->A->y, 
+		temp_bX=aFig->B->x - aFig->C->x, 
+		temp_bY=aFig->B->y - aFig->C->y, 
+		temp_cX=aFig->A->x - aFig->B->x,  
+		temp_cY=aFig->A->y - aFig->B->y;
+		
+		float temp_a=sqrt(temp_aX*temp_aX+temp_aY*temp_aY), 
+		temp_b=sqrt(temp_bX*temp_bX+temp_bY*temp_bY), 
+		temp_c=sqrt(temp_cX*temp_cX+temp_cY*temp_cY);
+		
+		if(temp_a>0) temp_a*=-1;
+		if(temp_b>0) temp_b*=-1;
+		if(temp_c>0) temp_c*=-1;
+		
+		if(temp_a==0 || temp_b==0 || temp_c==0) 
+		{
+			printf("Error. The two points coincide\n");
+			result=NULL;
+		}
+		else
+		{
+			float P=temp_a+temp_b+temp_c;
+			
+			if(P/3>temp_a && P/3>temp_b && P/3>temp_c)
+			{
+				float p=P/2;
+				result->S=sqrt(p*(p-temp_a)*(p-temp_b)*(p-temp_c));
+				
+				result->a=temp_a;
+				result->b=temp_b;
+				result->c=temp_c;
+			}
+			else 
+			{
+				printf("Error. All points lie on one straight line\n");
+				result=NULL;
+			}
+		}
+		
+	}
+	return result;
+}
+
+void printFig(Fig *aFig)
+{
+	if(NULL!=aFig->A && NULL!=aFig->B && NULL!=aFig->C)
+	{
+		printf("[Fig]:\nA(%d,%d)\nB(%d,%d)\nC(%d,%d)\n", aFig->A->x, aFig->A->y, aFig->B->x, aFig->B->y, aFig->C->x, aFig->C->y);
+	}
+}
+
+void printParam(Param *aParam)
+{
+	if(NULL!=aParam)
+	{
+		printf("[Param]:\na=%f;\nb=%f;\nc=%f;\nS=%f;\n", aParam->a, aParam->b, aParam->c, aParam->S);
+	}
+}
+
--- dk72/DruiDsta_Andrievskiy/DZ_2/main.c
+++ dk72/DruiDsta_Andrievskiy/DZ_2/main.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dru_lib.h"
 
+int main(int argc, char *argv[]) 
+{
+	Point A={0,0};
+	Point B={0,0};
+	Point C={0,0};
+	
+	printf("Enter A->x ");
+	scanf("%d",&A.x);
+	printf("Enter A->y ");
+	scanf("%d",&A.y);
+	printf("Enter B->x ");
+	scanf("%d",&B.x);
+	printf("Enter B->y ");
+	scanf("%d",&B.y);
+	printf("Enter C->x ");
+	scanf("%d",&C.x);
+	printf("Enter C->y ");
+	scanf("%d",&C.y);
+	
+	Fig *ABC=createFig(&A, &B, &C);
+	printFig(ABC);
+	
+	Param *ParamABC=enterParam(ABC);
+	printParam(ParamABC);
+	
+	destroyFig(ABC);
+	free(ParamABC);
+	
+	return 0;
+}
