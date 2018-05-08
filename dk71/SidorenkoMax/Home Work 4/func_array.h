0293ac0f384c317b8ff442ade536cdfb47948ce5
Max <32821682+WooDooUA@users.noreply.github.com>
2018-05-09 01:08:11 +0300

    Add files via upload

---
+++ dk71/SidorenkoMax/Home Work 4/func_array.h
@@ -0,0 +1,28 @@
+#ifndef func_array.h
+#define func_array.h
+
+
+#include <stdio.h>
+#include "func.h"
+
+typedef struct tagTriangleArray
+	{
+		int number;
+		int count;
+		float *square;
+		
+		Triangle **Triangles;
+	}TriangleArray;
+	
+	
+	
+	
+TriangleArray *createArray(int aNumber);
+void freeArray(TriangleArray *anArray);
+
+
+int addElement(TriangleArray *anArray, Triangle *aTriangle, float square);
+void writeArrayToJSON(FILE *aFile, TriangleArray *anArray);
+void insertion_sort(TriangleArray *anArray);
+
+#endif
