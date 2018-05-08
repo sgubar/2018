e804da83fe365e29afd7841f41c05f56a7c00986
Slavaromanenko3424 <36336001+Slavaromanenko3424@users.noreply.github.com>
2018-05-08 15:57:04 +0300

    Add files via upload

---
+++ dk71/Romanenko Svyatoslav/Laba 1/lb_1.h
@@ -0,0 +1,11 @@
+#ifndef lb_1_h
+#define lb_1_h
+
+#include <stdio.h>
+
+int factorial(int znach);
+int multiply(int znach1, int znach2);
+int division(int znach1, int znach2);
+int math_sum(int C, int A);
+
+#endif
