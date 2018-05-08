505a302c6e79be03b826d1735a42f393a677b09e
tim29000 <36280587+tim29000@users.noreply.github.com>
2018-05-08 18:16:35 +0300

    Create dk_lab2.h

---
+++ dk71/tim29000/Lab2/dk_lab2.h
@@ -0,0 +1,14 @@
+#ifndef dk_lab2_h
+#define dk_lab2_h
+#include <stdio.h>
+
+
+void Type_chooser( int mass[], int quantity, int type);
+void Bubble(int mass[], int quantity);
+void Select(int mass[], int quantity);
+void Insert(int mass[], int quantity);
+void swap (int *x, int *y);
+void random(int mass [], int quantity);
+void Filewriter(int mass[], int quantity, char* name);
+
+#endif
