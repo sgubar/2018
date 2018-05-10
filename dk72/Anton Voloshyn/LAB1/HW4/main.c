65a7c7a45e0eb11c4d1ce1c58a9ecf1f9af78486
LoveFELAnoniy <36298304+LoveFELAnoniy@users.noreply.github.com>
2018-05-10 12:56:31 +0300

    Add files via upload

---
+++ dk72/Anton Voloshyn/LAB1/HW4/main.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dk_tool.h"
+#include "lab2.h"
+
+int main()
+{
+    ArrayOfTriangle *theArray = createArray(3);
+    
+    Point A = {4,9};
+    Point B = {-2,5};
+    Point C = {-2,9};
+    
+    Point X = {-5,5};
+    Point Y = {-1,2};
+    Point Z = {-1,5};
+    
+    Triangle *ABC = createTriangle(&A, &B, &C);
+    Triangle1 *XYZ = createTriangle(&X, &Y, &Z);
+    if(ABC == NULL && XYZ == NULL)
+    {
+        printf("oop, error\n");
+    }
+    else
+    {
+        printf("Square(ABC) = %.2f\n", (float)AreaOfTriangle(ABC));
+        printf("Square(XYZ) = %.2f\n", (float)AreaOfTriangle(XYZ));
+    }
+    FILE *theFile = fopen("file.json", "a+");
+    writeFileToJSON(theFile, theArray);
+    fflush(theFile);
+	fclose(theFile);
+    
+    freeArray(theArray);
+
+    
+	Bubble_Sort(theArray);
+
+
+	FILE *theFile2 = fopen("D:/file_sort.json", "a+");
+
+	writeArrayToJSON(theFile2, theArray);
+
+	fflush(theFile2);
+	fclose(theFile2);
+
+	freeArray(theArray);
+
+return 0;
+}
+
