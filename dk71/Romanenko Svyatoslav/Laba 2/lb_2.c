63aab5bb3e9b06e48a86a59d4ce8c58dd90c1d95
Slavaromanenko3424 <36336001+Slavaromanenko3424@users.noreply.github.com>
2018-05-12 14:45:36 +0300

    Add files via upload

---
+++ dk71/Romanenko Svyatoslav/Laba 2/lb_2.c
@@ -0,0 +1,118 @@
+#include "lb_2.h"
+
+int enter_size_array()
+{
+	int size;
+	printf("Enter a size of array(0-2 147 483 647): ");
+	scanf("%d", &size);
+	printf("You have entered: %d\n", size);
+	
+	while(size <= 0 || size > 2147483647)
+	{
+		printf("ERR Invalid size of array:(\nEnter new: ");
+		scanf("%d", &size);
+		printf("You have entered: %d\n", size);
+	}
+	return size;
+}
+
+void enter_data_to_array(char *array, int size)
+{
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		if(rand()%2 == 1) array[i] = rand()%('z' + 1 - 'a')+'a';
+		else array[i] = rand()%('9' + 1 - '0')+'0';	
+	}
+}
+
+char* loadFile (char* path , int  size) {
+  FILE *infile = fopen (path, "r");
+  if (infile !=NULL)
+    {
+    int i = 0;
+    char *output_array = malloc (sizeof (char) * (size+1));
+    for (i = 0;i < size;i++)
+    {
+      fscanf (infile, "%c", &output_array[i]);
+    }
+    fclose (infile);
+    
+    return output_array;
+  }
+  return NULL;
+}
+
+void write_array_in_fail(char *aray, int size, char *name)
+{
+	FILE *file;
+	file= fopen(name, "w+");
+	int i;
+	for(i = 0; i < size; i++)
+	{
+	    fprintf(file, "%c", aray[i]);
+
+	}
+
+	fclose(file);
+}
+
+void sort_bulbashka(char *aray, int size)
+{
+	int k,i;
+  for(k = 0; k < size - 1; k++)
+  {
+    int swaps = 0;
+    for( i = 0; i < size - 1 - k; i++)
+      {
+        if(aray[i] < aray[i+1])
+        {
+          int temp = aray[i+1];
+          aray[i+1] = aray[i];
+          aray[i] = temp;
+          swaps++;
+        }
+      }
+    if(!swaps)
+      break;
+  }
+}
+
+void sort_vstavka(char *aray, int size)
+{
+    char newElement;
+    int  location,i;
+
+    for ( i = 1; i < size; i++)
+    {
+        newElement = aray[i];
+        location = i - 1;
+        while(location >= 0 && aray[location] < newElement)
+        {
+            aray[location+1] = aray[location];
+            location = location - 1;
+        }
+        aray[location+1] = newElement;
+    }
+}
+
+void sort_vybora(char *aray, int size)
+{
+    char temp;
+    int dopomigna,i,j;
+    for(i = 0; i < size; ++i)
+    {
+        dopomigna = i;
+        temp = aray[i];
+        for(j = i + 1; j < size; ++j)
+        {
+            if (aray[j] > temp)
+            {
+               dopomigna = j;
+               temp = aray[j];
+            }
+        }
+        aray[dopomigna] = aray[i];
+        aray[i] = temp;
+    }
+}
---
+++ dk71/Romanenko Svyatoslav/Laba 2/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lb_2.h"
+
+int main(int argc, char *argv[]) {
+	
+	float TimeStart,TimeStop;
+	int size=0;
+	size = enter_size_array();
+	char *array = malloc (sizeof (char) * (size+1));
+	enter_data_to_array(array, size);
+	write_array_in_fail(array, size, "lab2.txt");
+
+	char * bulbashka = loadFile("lab2.txt" , size);
+
+	printf("sorting......\n");
+
+	TimeStart = clock()/(float)CLOCKS_PER_SEC;
+	sort_bulbashka(bulbashka, size);
+	TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+	printf("\n sorting bulbashkoj: %f \n", TimeStop-TimeStart);
+
+	char * vstavka = loadFile("lab2.txt" , size);
+
+	TimeStart = clock()/(float)CLOCKS_PER_SEC;
+	sort_vstavka(vstavka, size);
+	TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+	printf("\n sorting vstavkoj: %f \n", TimeStop-TimeStart);
+
+	char * vybor = loadFile("lab2.txt" , size);
+
+	TimeStart = clock()/(float)CLOCKS_PER_SEC;
+	sort_vybora(vybor, size);
+	TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+	printf("\n sorting vyborom: %f \n", TimeStop-TimeStart);
+	
+	write_array_in_fail(bulbashka, size, "bulbashka.txt");
+	write_array_in_fail(vstavka, size, "vstavka.txt");
+	write_array_in_fail(vybor, size, "vybor.txt");
+
+
+	free(bulbashka);
+	free(vstavka);
+	free(vybor);
+	free(array);
+
+	return 0;
+}
