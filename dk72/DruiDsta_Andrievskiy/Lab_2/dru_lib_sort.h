32f796dadc23b384e6a342a887d2566dbbc67862
DruiDsta <33695583+DruiDsta@users.noreply.github.com>
2018-05-13 12:09:20 +0300

    Add files via upload

---
+++ dk72/DruiDsta_Andrievskiy/Lab_2/dru_lib_sort.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <conio.h>
+//#include <string.h>
+
+void EnterMass(int *mass, int size);
+void PrintMass(int *mass, int size);
+void TMP(int *A1, int *A2);
+void bubbleSort(int *anArray, int aSize);
+void selectionSort(int *anArray, int aSize);
+void insertionSort(int *anArray, int aSize);
+float ClockBubbleSort(int *anArray, int aSize);
+float ClockSelectionSort(int *anArray, int aSize);
+float ClockInsertionSort(int *anArray, int aSize);
+void WriteMass(int *mass, int size, float time, char *name, char *sort);
--- dk72/DruiDsta_Andrievskiy/Lab_2/main.c
+++ dk72/DruiDsta_Andrievskiy/Lab_2/main.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dru_lib_sort.h"
 
+int main(void)
+{
+	int size;
+	printf("Enter the quantity of numbers in the array:");
+	scanf("%d",&size);
+	if(size<0) size=-size;
+	int mass[size];
+	
+	//BubbleSort
+	EnterMass(mass, size);
+	PrintMass(mass, size);
+	float TimeBubbleSort;
+	TimeBubbleSort=ClockBubbleSort(mass, size);
+	PrintMass(mass, size);
+	printf("TimeBubbleSort=%.15f\n", TimeBubbleSort);
+	WriteMass(mass, size, TimeBubbleSort, "Bubble_Sort.txt", "Bubble Sort");
+	system("pause");
+	
+	//SelectionSort
+	EnterMass(mass, size);
+	PrintMass(mass, size);
+	float TimeSelectionSort;
+	TimeSelectionSort=ClockSelectionSort(&mass, size);
+	PrintMass(mass, size);
+	printf("TimeSelectionSort=%.15f\n", TimeSelectionSort);
+	WriteMass(mass, size, TimeSelectionSort, "Selection_Sort.txt", "Selection Sort");
+	system("pause");
+	
+	//InsertionSort
+	EnterMass(mass, size);
+	PrintMass(mass, size);
+	float TimeInsertionSort;
+	TimeInsertionSort=ClockInsertionSort(&mass, size);
+	PrintMass(mass, size);
+	printf("TimeInsertionSort=%.15f\n", TimeInsertionSort);
+	WriteMass(mass, size, TimeInsertionSort, "Insertion_Sort.txt", "Insertion Sort");
+	system("pause");
+	
+	return 0;
+}
