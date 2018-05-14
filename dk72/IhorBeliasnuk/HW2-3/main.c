581ead5fd77732a4dc1f11fa5f035ea54e04ab1e
ihorbeliasnuk <dynamite588@gmail.com>
2018-05-15 00:45:22 +0300

    Add files via upload

--- dk72/IhorBeliasnuk/HW2-3/main.c
+++ dk72/IhorBeliasnuk/HW2-3/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "dk_rectangle.h"
 #include "dk_array.h"
+#include "dk_sort.h"
 
 int main()
 {
@@ -16,23 +17,36 @@ int main()
     Point Z = {2, -2};
     Point K = {-2, -2};
 
+    Point F = {-4, 4};//declaring points
+    Point G = {4, 4};
+    Point H = {4, -4};
+    Point J = {-4, -4};
+
     Rectangle *example1 = createRectangle(&A, &B, &C, &D);
     Rectangle *example2 = createRectangle(&X, &Y, &Z, &K);
+    Rectangle *example3 = createRectangle(&F, &G, &H, &J);
 
-    RectangleArray *RectBase = createArray(2);
+    RectangleArray *RectBase = createArray(3);
 
-    addElement(RectBase, example1);
+    addElement(RectBase, example3);
     addElement(RectBase, example2);
+    addElement(RectBase, example1);
 
-    FILE *file = fopen("rectbase.json", "a+");
+    FILE *file1 = fopen("rectbase_unsorted.json", "w");
 
-    writeArrayToJSON(file, RectBase);
+    writeArrayToJSON(file1, RectBase);
+
+    fclose(file1);
+
+    selectionSort(RectBase, RectBase->count);
+
+    FILE *file2 = fopen("rectbase_sorted.json", "w");
+
+    writeArrayToJSON(file2, RectBase);
+
+    fclose(file2);
 
     freeArray(RectBase);
-    
-    fflush(file);
-    
-    fclose(file);
 }
 
 
