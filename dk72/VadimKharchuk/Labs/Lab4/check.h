d76455b1d5c784f265b29e130711489aefe92209
M0rel <36273999+M0rel@users.noreply.github.com>
2018-05-15 21:49:41 +0300

    Create check.h

---
+++ dk72/VadimKharchuk/Labs/Lab4/check.h
@@ -0,0 +1,31 @@
+//
+// Created by vadim on 15.05.2018.
+//
+
+#ifndef LAB4_CHECK_H
+#define LAB4_CHECK_H
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct __tagCharNode CharNode;
+struct __tagCharNode
+{
+    int value;
+    CharNode *prev;
+    CharNode *next;
+};
+
+typedef struct __tagCharList
+{
+    CharNode *head;
+    CharNode *tail;
+    char symbol;
+    int count;
+
+}CharList;
+
+CharList *CreateCharList();
+void check(CharList *aList,CharList *bList, FILE *aFile);
+void addValueToList(CharList *aList,char counter);
+CharNode *createCharNodeWithValue (int anCharValue);
+#endif //LAB4_CHECK_H
