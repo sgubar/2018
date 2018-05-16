672312088617dec5754cea1cc4285279c01e1249
ihorbeliasnuk <dynamite588@gmail.com>
2018-05-17 01:09:25 +0300

    Update main.c

--- dk72/IhorBeliasnuk/HW2-3-4/main.c
+++ dk72/IhorBeliasnuk/HW2-3-4/main.c
@@ -22,15 +22,29 @@ int main()
     Point H = {4, -4};
     Point J = {-4, -4};
 
+    Point Ab = {-5, 5};
+    Point Ac = {5, 5};
+    Point Ad = {5, -5};
+    Point Ae = {-5, -5};
+
+    Point AA = {-40, 40};
+    Point BB = {40, 40};
+    Point CC = {40, -40};
+    Point DD = {-40, -40};
+
     Rectangle *example1 = createRectangle(&A, &B, &C, &D);
     Rectangle *example2 = createRectangle(&X, &Y, &Z, &K);
     Rectangle *example3 = createRectangle(&F, &G, &H, &J);
+    Rectangle *example4 = createRectangle(&Ab, &Ac, &Ad, &Ae);
+    Rectangle *example5 = createRectangle(&AA, &BB, &CC, &DD);
 
-    RectangleArray *RectBase = createArray(3);
+    RectangleArray *RectBase = createArray(5);
 
     addElement(RectBase, example3);
     addElement(RectBase, example2);
     addElement(RectBase, example1);
+    addElement(RectBase, example5);
+    addElement(RectBase, example4);
 
     FILE *file1 = fopen("rectbase_unsorted.json", "w");
 
@@ -38,7 +52,20 @@ int main()
 
     fclose(file1);
 
-    selectionSort(RectBase, RectBase->count);
+    int select;
+    printf("\nEnter 1 for selection sort, 2 - for Shell sort\n");
+    scanf("%d", &select);
+
+    if(select == 1)
+    {
+        selectionSort(RectBase, RectBase->count);
+    }
+    if(select == 2)
+    {
+        shellSort(RectBase, RectBase->count);
+    }
+
+    printf("\nresult - %d", binarySearch(64, RectBase, RectBase->count));
 
     FILE *file2 = fopen("rectbase_sorted.json", "w");
 
