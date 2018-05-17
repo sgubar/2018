3327e6f76076dd6f0b06877fa80162ff09a2a990
M0rel <36273999+M0rel@users.noreply.github.com>
2018-05-17 11:51:30 +0300

    Create sort.h

---
+++ dk72/VadimKharchuk/HW5/sort.h
@@ -0,0 +1,10 @@
+//
+// Created by vadim on 17.05.2018.
+//
+
+#ifndef HW5_SORT_H
+#define HW5_SORT_H
+#include "dk_elipse_array.h"
+void shell_sort(ElipsArray *anArray);
+void lineSeach (ElipsArray *anArray, float);
+#endif //HW5_SORT_H
