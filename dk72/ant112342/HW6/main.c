1132907e9c016d1d036c3b55013e68996d985302
ant112342 <36268347+ant112342@users.noreply.github.com>
2018-05-18 00:40:54 +0300

    Create main.c

---
+++ dk72/ant112342/HW6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dk_node.h"
+#include "dk_list.h"
+#include"menu.h"
+#include "quickSort.h"
+
+
+int main(int argc, const char * argv[])
+{
+	doTestSLList();
+
+	return 0;
+}
+
