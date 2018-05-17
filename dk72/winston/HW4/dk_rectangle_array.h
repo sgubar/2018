7db382ac42305294cfd38d38188b5986f2eadfc2
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-05-17 13:13:01 +0300

    Create dk_rectangle_array.h

---
+++ dk72/winston/HW4/dk_rectangle_array.h
@@ -0,0 +1,26 @@
+#ifndef DK_RECTANGLE_ARRAY_H_INCLUDED
+#define DK_RECTANGLE_ARRAY_H_INCLUDED
+
+#include "dk_rectangle.h"
+
+typedef struct tagRectangleArray
+{
+	int number;
+	int count;
+
+	Rectangle **Rectangles;
+
+}RectangleArray;
+
+
+RectangleArray *createArray(int aNumber);
+void freeArray(RectangleArray *anArray);
+
+
+int addElement(RectangleArray *anArray, Rectangle *aRectangle);
+void writeArrayToJSON(FILE *aFile, RectangleArray *anArray);
+
+void sort_buble(RectangleArray *anArray);
+
+
+#endif // DK_RECTANGLE_ARRAY_H_INCLUDED
