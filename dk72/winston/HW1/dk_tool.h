59cc291d50022cb949d508e9836ce9944074c3b5
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-05-17 13:00:42 +0300

    Create dk_tool.h

---
+++ dk72/winston/HW1/dk_tool.h
@@ -0,0 +1,13 @@
+#ifndef DK_TOOLS_H_INCLUDED
+#define DK_TOOLS_H_INCLUDED
+
+#include <stdio.h>
+
+
+int* MatrixSubtraction ( int *A , int *B ,int rows , int colums );
+
+void MatrixOutPut (int* M , int rows , int colums);
+
+void InputMatrix( int* M , int rows , int colums);
+
+#endif // DK_TOOLS_H_INCLUDED
