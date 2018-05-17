d77d46559e3f0b2e3c9765aefba72aaf16574707
LoveFELAnoniy <36298304+LoveFELAnoniy@users.noreply.github.com>
2018-05-17 12:59:38 +0300

    Add files via upload

---
+++ dk72/Anton Voloshyn/LAB1/HW6/dk_tool.h
@@ -0,0 +1,38 @@
+
+#ifndef dk_tool_h
+#define dk_tool_h
+
+#include <stdio.h>
+
+typedef struct __doubleNode doubleNode;
+
+typedef struct __doubleList
+{
+    doubleNode *head;
+    doubleNode *tail;
+    double count;
+}doubleList;
+struct __doubleNode
+{
+    double value;
+    doubleNode *nextNode;//
+};
+doubleNode *SLCreateNodeWithdoubleValue(double aValue);
+void SLFreedoubleNode(doubleNode *aNode);
+
+extern const double kSLListError;
+
+doubleList *SLCreateList();
+void SLFreeList(doubleList *aList);
+
+doubleNode *SLAddNode(doubleList *aList, doubleNode *aNewNode);
+
+double SLCountList(const doubleList *aList);
+doubleNode *SLNodeAtIndex(const doubleList *aList, int aIndex);
+
+doubleNode *SLInsertNodeAtIndex(doubleList *aList, doubleNode *aNewNode, int anIndex);
+doubleNode *SLRemovedNodeAtIndex(doubleList *aList, int anIndex);
+
+void SLSwapNodesByIndex(doubleList *aList, int aLeftIndex, int aRightIndex);
+
+#endif /* dk_tool */
