4985150cf25184d8d2ef0f6b27ec1e5b3014a457
John <33196127+WIllsnSmith@users.noreply.github.com>
2018-05-18 04:44:32 +0300

    Create dk_sort.h

---
+++ dk72/winston/LABA3/dk_sort.h
@@ -0,0 +1,21 @@
+#ifndef DK_SORT_H_INCLUDED
+#define DK_SORT_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <conio.h>
+
+
+void quickSort( char *array, int l, int r);
+int partition( char *array, int l, int r);
+int binarysearch(char key, char *array, int size) ;
+void output_of_the_array(char* x, int size);
+void record_to_file(char *aray, int size, char *name);
+int GetLetterAmoutInFile (FILE* file);
+int GetCharsAmoutInFile (FILE* file);
+char* loadFile (char * path , int * size);
+
+
+#endif // DK_SORT_H_INCLUDED
