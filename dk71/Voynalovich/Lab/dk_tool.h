c76da24951ea11785be7fdb06124840dd5487bed
Voinstar <36274842+Voinstar@users.noreply.github.com>
2018-05-18 10:55:48 +0300

    Delete dk_tool.h

--- dk71/Voynalovich/Lab/dk_tool.h
+++
@@ -1,7 +0,0 @@
-#ifndef DK_TOOL_H_INCLUDED
-#define DK_TOOL_H_INCLUDED
-
-int factorial(int n);
-int functsiaSumy(int chislo_A, int chislo_Q , int (*Yfactorial)(int));
-
-#endif // DK_TOOL_H_INCLUDED
