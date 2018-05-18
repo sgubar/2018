a8e36303c3438630c193f140c50bc703ef30b291
John <33196127+WIllsnSmith@users.noreply.github.com>
2018-05-18 04:42:02 +0300

    Create main.c

---
+++ dk72/winston/LABA2/main.c
@@ -0,0 +1,43 @@
+#include "dk_sort.h"
+
+int main()
+{
+float TimeStart,TimeStop;
+int size=0;
+char *buble = loadFile("lab_2.txt" , &size);
+
+printf("Sort!!!\n");
+
+TimeStart = clock()/(float)CLOCKS_PER_SEC;
+buble_sort(buble, size);
+TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+printf("\nBuble Sort: %f \n", TimeStop-TimeStart);
+
+char * insert = loadFile("lab_2.txt" , &size);
+
+TimeStart = clock()/(float)CLOCKS_PER_SEC;
+insert_sort(insert, size);
+TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+printf("\nInsert Sort: %f \n", TimeStop-TimeStart);
+
+char * selection = loadFile("lab_2.txt" , &size);
+
+TimeStart = clock()/(float)CLOCKS_PER_SEC;
+selection_sort(selection, size);
+TimeStop = clock()/(float)CLOCKS_PER_SEC;
+
+printf("\nSelection Sort: %f \n", TimeStop-TimeStart);
+
+record_to_file(buble, size, "buble.txt");
+record_to_file(insert, size, "insert.txt");
+record_to_file(selection, size, "selection.txt");
+printf("\nView the created files!!!:)\n");
+
+free(buble);
+free(insert);
+free(selection);
+
+return 0;
+}
