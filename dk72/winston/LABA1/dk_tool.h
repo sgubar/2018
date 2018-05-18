d1d2a78094e7ef092d90dfe7652d6a810a611dad
John <33196127+WIllsnSmith@users.noreply.github.com>
2018-05-18 04:41:14 +0300

    Create dk_tool.h

---
+++ dk72/winston/LABA1/dk_tool.h
@@ -0,0 +1,7 @@
+#ifndef DK_TOOL_H_INCLUDED
+#define DK_TOOL_H_INCLUDED
+
+void Enter_numbers(int *number_A, int *number_B, int *number_C);
+int function(int number_A, int number_B, int number_C);
+
+#endif // DK_TOOL_H_INCLUDED
