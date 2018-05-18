2b57b55c8854390ac035baee0c92e86c43b0c201
LivesPain <36304357+LivesPain@users.noreply.github.com>
2018-05-18 14:56:37 +0300

    Create main.c

---
+++ dk72/Kononenko V/lab1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "LB1.h"
+
+int main (int argc, char *argv[])
+{
+	printf("\nStart of programm,enter value\n");
+	
+	float A, B, C, D;
+	float nomin, denomin, frac, result;
+	
+	
+	// Ask the user to enter values
+	printf("\nA = ");
+	scanf_s("%f", &A);
+	printf("\nB = ");
+	scanf_s("%f", &B);
+	printf("\nC = ");
+	scanf_s("%f", &C);
+	int i=0;
+	for(;i<B;i++){
+		D=factorial(i)+D;	
+		
+	}
+	
+	//Formula 
+	nomin = (A*B);
+	denomin = (B + C*C);
+	frac =  nomin/denomin;
+	result = frac + D;
+	if(denomin==0){
+		printf("\n func of domination cant be 0");
+		return;}
+		else{
+	
+		
+	
+	printf("\n Result =  %.2f \n", result);	
+	
+        }
+	
+	getch();
+	return 0;
+}
