98678967ca125abd1a58d83d8fcf6cd5cdd78a68
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-05-24 01:46:56 +0300

    Update dk_triangle_array.h

--- dk72/PasiutaDmitriy/HW3/dk_triangle_array.h
+++ dk72/PasiutaDmitriy/HW3/dk_triangle_array.h
@@ -4,8 +4,10 @@
 // Created by dima on 26.04.18.
 //
 
-#ifndef HW3_DK_TRIANGLE_ARRAY_H
-#define HW3_DK_TRIANGLE_ARRAY_H
+
+#ifndef DMITRY_DK_TRIANGLE_ARRAY_H
+#define DMITRY_DK_TRIANGLE_ARRAY_H
+
 #include <stdio.h>
 #include "dk_triangle.h"
 
@@ -13,10 +15,25 @@ typedef struct tagTriangleArray
 {
     int number;
     int count;
-
     Triangle **triangles;
 }TriangleArray;
 
+/*
+{
+	"number" : (int),
+	"count" : (int),
+	"lines" :
+	[
+ 		 {
+ 			"A" : {"x" : (int), "y" : (int)},
+		 	"B" : {"x" : (int), "y" : (int)}
+		 },
+ },
+ 		{},
+ 		....
+	]
+}
+*/
 
 // init/dealloc
 TriangleArray *createArray(int aNumber);
@@ -26,6 +43,5 @@ void freeArray(TriangleArray *anArray);
 // returns index of added element OR -1 if fail
 int addElement(TriangleArray *anArray, Triangle *atriangle);
 void writeArrayToJSON(FILE *aFile, TriangleArray *anArray);
-
-
-#endif //HW3_DK_TRIANGLE_ARRAY_H
+void insertion_sort(TriangleArray *anArray);
+#endif //DMITRY_DK_TRIANGLE_ARRAY_H
