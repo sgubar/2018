f1dd6633b30ac9a53cf963a8e6f89b93825b2206
NikolayKrushin <36264396+NikolayKrushin@users.noreply.github.com>
2018-05-23 11:49:52 +0300

    Create SortDR6V2.h

---
+++ dk72/Ny_Ok/DZ 6/SortDR6V2.h
@@ -0,0 +1,9 @@
+#ifndef SORTDR6V2_H_INCLUDED
+#define SORTDR6V2_H_INCLUDED
+#include "DR_6_V2.h"
+
+void sPrintList(const FloatList *aList);
+void SwapNodesByIndex(FloatList *aList, int i_Index, int j_Index);
+void ShellsSort(FloatList *aList);
+
+#endif // SORTDR6V2_H_INCLUDED
