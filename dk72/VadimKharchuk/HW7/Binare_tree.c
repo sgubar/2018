4bf664e2c32f821d8181fc4c243a74b8bbf8b133
M0rel <36273999+M0rel@users.noreply.github.com>
2018-05-23 09:49:44 +0300

    Create Binare_tree.c

---
+++ dk72/VadimKharchuk/HW7/Binare_tree.c
@@ -0,0 +1,194 @@
+//
+// Created by vadim on 20.05.2018.
+//
+
+#include "Binare_tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+static void destroyNode(IntNode *aNode);
+static IntNode *createIntNodeWithValue(int aValue);
+
+IntTree *createIntTree()
+{
+    IntTree *theTree = (IntTree *)malloc(sizeof(IntTree));
+
+    if (NULL != theTree)
+    {
+        theTree->root = NULL;
+        theTree->count = 0;
+    }
+    return theTree;
+}
+
+void destroyIntTree(IntTree *aTree)
+{
+    if (NULL != aTree)
+    {
+        destroyNode(aTree->root);
+        free(aTree);
+    }
+}
+
+void insertIntValueToTree(IntTree *aTree, int aValue)
+{
+    if (NULL == aTree)
+    {
+        return;
+    }
+
+    IntNode *theNode = createIntNodeWithValue(aValue);
+    if (NULL == theNode)
+    {
+        return;
+    }
+
+    if (NULL == aTree->root)
+    {
+        aTree->root = theNode;
+        aTree->count ++;
+    }
+    else
+    {
+        IntNode *theCurrent = aTree->root;
+        IntNode *theParent = NULL;
+
+        while (1)
+        {
+            theParent = theCurrent;
+
+            if (aValue < theCurrent->value)
+            {
+                theCurrent = theCurrent->leftChild;
+                if (NULL == theCurrent)
+                {
+                    theParent->leftChild = theNode;
+                    break;
+                }
+            }
+            else
+            {
+                theCurrent = theCurrent->rightChild;
+                if (NULL == theCurrent)
+                {
+                    theParent->rightChild = theNode;
+                    break;
+                }
+            }
+        }
+        aTree->count ++;
+    }
+}
+
+IntNode *findNodeWithValue(IntTree *aTree, int aValue)
+{
+    IntNode *theCurrentNode = NULL;
+    if (NULL != aTree)
+    {
+        IntNode *theCurrentNode = aTree->root;
+        while (aValue != theCurrentNode->value)
+        {
+            theCurrentNode = (aValue < theCurrentNode->value)
+                             ? theCurrentNode->leftChild
+                             : theCurrentNode->rightChild;
+            if (NULL == theCurrentNode)
+            {
+                break;
+            }
+        }
+        return theCurrentNode;
+    }
+    return theCurrentNode;
+}
+
+
+void destroyNode(IntNode *aNode)
+{
+    if (NULL != aNode)
+    {
+        destroyNode(aNode->leftChild);
+        destroyNode(aNode->rightChild);
+
+        free(aNode);
+    }
+}
+
+IntNode *createIntNodeWithValue(int aValue)
+{
+    IntNode *theNode = (IntNode *)malloc(sizeof(IntNode));
+
+    if (NULL != theNode)
+    {
+        theNode->leftChild = NULL;
+        theNode->rightChild = NULL;
+        theNode->value = aValue;
+    }
+    return theNode;
+}
+
+void inOrder(IntNode *aNode)
+{
+    if(aNode != NULL)
+    {
+        inOrder(aNode ->leftChild);
+        printf("%d ",aNode ->value);
+        inOrder(aNode -> rightChild);
+    }
+}
+void printTree (IntTree *aTree)
+{
+    IntNode *theCurrentNode = aTree->root;
+    if(aTree != NULL)
+    {
+        inOrder(theCurrentNode);
+    }
+}
+
+IntNode* FindMin(IntNode* aNode)
+{
+    while (aNode->leftChild != NULL) aNode = aNode->leftChild;
+    return aNode;
+}
+
+IntNode* deleteNodeWithElement(IntTree *aDrevo,IntNode* aNode, int aElement)
+{
+    if (aNode == NULL) {
+        return NULL;
+    }
+    else if (aElement < aNode->value)
+        aNode->leftChild = deleteNodeWithElement(aDrevo,aNode->leftChild, aElement);
+    else if (aElement > aNode->value)
+        aNode->rightChild = deleteNodeWithElement(aDrevo , aNode->rightChild, aElement);
+    else
+    {
+        if (aNode->leftChild == NULL && aNode->rightChild == NULL)
+        {
+            free(aNode);
+            aNode = NULL;
+            aDrevo->count--;
+        }
+        else if (aNode->leftChild == NULL)
+        {
+            IntNode *theTmp = aNode;
+            aNode = aNode->rightChild;
+            free(theTmp);
+            aDrevo->count--;
+        }
+        else if (aNode->rightChild == NULL)
+        {
+            IntNode *theTmp = aNode;
+            aNode = aNode->leftChild;
+            free(theTmp);
+            aDrevo->count--;
+        }
+        else
+        {
+            IntNode *theTmp = FindMin(aNode->rightChild);
+            aNode->value = theTmp->value;
+            aNode->rightChild = deleteNodeWithElement(aDrevo, aNode->rightChild,theTmp->value);
+            aDrevo->count--;
+        }
+    }
+    return aNode;
+}
