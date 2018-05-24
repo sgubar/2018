6d705278c77b6149ba6882243d772b3fb24782a1
LoveFELAnoniy <36298304+LoveFELAnoniy@users.noreply.github.com>
2018-05-24 12:26:47 +0300

    Add files via upload

---
+++ dk72/Anton Voloshyn/LAB1/dk_sort.c
@@ -0,0 +1,200 @@
+#include "dk_sort.h"
+
+
+
+void shellSort(char *anArray, int aCount)
+
+{
+
+	int theInner = 0;
+
+	int theOuter = 0;
+
+	int theH = 1; // start h = 1
+
+
+
+	//1. Calculate start value of h
+
+	while (theH <= aCount/3)
+
+	{
+
+		theH = theH*3 + 1; // 1, 4, 13, 40, 121, ....
+
+	}
+
+
+
+	//2. Sequental decrease h to 1
+
+	while (theH > 0)
+
+	{
+
+		for (theOuter = theH; theOuter < aCount; theOuter ++)
+
+		{
+
+			int theTmp = anArray[theOuter];
+
+			theInner = theOuter;
+
+
+
+			// the first sub-array {0, 4, 8}
+
+			while (theInner > theH-1 && anArray[theInner - theH] >= theTmp)
+
+			{
+
+				anArray[theInner] = anArray[theInner - theH];
+
+				theInner -= theH;
+
+			}
+
+
+
+			anArray[theInner] = theTmp;
+
+		}
+
+
+
+		theH = (theH - 1) / 3; //decrease h
+
+	}
+
+}
+
+
+
+
+
+
+int binary_find(char *theArray, char search, int size)
+{
+	int i, left, right;
+	left = 0; right = size-1;
+	while (1)
+ 	{
+  	  if (left > right) return (-1);
+  	  i = left + (right - left) / 2;
+  	  if (theArray[i] < search) left = i + 1;
+   	  if (theArray[i]> search) right = i - 1;
+   	  if (theArray[i] == search) return i+1;
+ 	}
+}
+
+
+
+
+
+int GetCharsAmoutInFile (FILE* file) {
+
+  int size = 0;
+
+  if (file != NULL) {
+
+    fseek (file, 0, SEEK_END);
+
+    size = ftell (file);
+
+    fseek (file, 0, SEEK_SET);
+
+    return size-1;
+
+  }
+
+  else {
+
+    return -1;
+
+  }
+
+}
+
+
+
+char* loadFile (char * path , int  *size) {
+
+  FILE *infile = fopen (path, "r");
+
+  if (infile !=NULL)
+
+    {
+
+    int 1size = GetCharsAmoutInFile (infile);
+
+    int i = 0;
+
+    char * o = malloc (sizeof (char) * (1size+1));
+
+    for (int i = 0;i < 1size;i++)
+
+    {
+
+      fscanf (infile, "%c", &o[i]);
+
+    }
+
+    fclose (infile);
+
+    *size = 1size;
+
+    return o;
+
+  }
+
+  return NULL;
+
+}
+
+
+
+void inFile(char *aray, int size, char *name)
+
+{
+
+	FILE *file;
+
+	file= fopen(name, "w+");
+
+
+
+	for(int i = 0; i < size; i++)
+
+	{
+
+
+
+	    fprintf(file, "%c", aray[i]);
+
+
+
+	}
+
+
+
+	fclose(file);
+
+}
+
+
+
+void outArr(char* x, int size)
+
+{
+
+	for (int i = 0; i < size; i++)
+
+	{
+
+		printf("%c", x[i]);
+
+	}
+
+	printf("\n");
+
+}
--- dk72/Anton Voloshyn/LAB1/main.c
+++ dk72/Anton Voloshyn/LAB1/main.c
@@ -1,59 +1,39 @@
-#include <stdio.h>
+#include "dk_sort.h"
 
-#include "dk_tool.h"               
-
-
-int main(int argc,char* argv[])
-
+int main(int argc, const char * argv[])
 {
-
-int A, B, C;  
-float Q = 0;                     
-
-printf("Enter your value A :\n");
-	while(scanf("%d", &A)!=1) {
-		while(getchar()!='\n');
-		printf("You entered not a value. Enter your value again! \n");   		// Check your mind, for stupidity for value A
-		}
-
-printf("Enter your value B :\n");
-	while(scanf("%d", &B)!=1) {
-		while(getchar()!='\n');
-		printf("You entered not a value. Enter your value again! \n");			// Check your mind, for stupidity again for value B
-		}
-
-printf("Enter your value C :\n");
-	while(scanf("%d", &C)== 0) {
-		while(getchar()!='\n');
-		printf("You entered not a value. Enter your value again! \n");			// Check your mind, for stupidity again for value C
-		}
-		while ((100 - B - C) == 0)
+	float TimeStart,TimeStop;
+	int size=0,number=0;
+	char symvol;
+	char * sorting_shell = loadFile("lab3.txt" , &size);
+	
+	
+	
+	TimeStart = clock()/(float)CLOCKS_PER_SEC;
+	shellSort(sorting_shell, size);
+	TimeStop = clock()/(float)CLOCKS_PER_SEC;
+	printf("\nTime of Quick sorting: %.10f sec!\n", TimeStop-TimeStart);
+	printf("\nEnter searching symbol: ");
+	scanf("%c",&symvol);
+	
+	TimeStart = clock()/(float)CLOCKS_PER_SEC;
+	number = binary_find(sorting_shell, symvol, size);
+	TimeStop = clock()/(float)CLOCKS_PER_SEC;
+	
+	if(number==-1)
 		{
-			printf ("Denominator can't be 0'");
-			return 0;
+		    printf("\nCan't find it!\n");
 		}
-	Q =(float) (A + B + C)/(100 - B - C) + SecondFromSumm(A);   
-               
-	printf("\n Your Q = %.2f\n", Q);     
+	else
+		{
+		    printf("\nFounded at number: %d \n", number);
+		}
+	
+	printf("\nTime of Binary find: %.10f sec!\n", TimeStop-TimeStart);
+	inFile(sorting_shell, size, "shell.txt");
+	printf("\nCheck lab3.txt:)\n");
+	
+	free(sorting_shell);
+	
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
