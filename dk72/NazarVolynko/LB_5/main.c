1531cb3670d3c8d600262a2c21b19566882a5df0
NazarVolynko <36282087+NazarVolynko@users.noreply.github.com>
2018-05-31 03:11:13 +0300

    Add files via upload

---
+++ dk72/NazarVolynko/LB_5/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "tree.h"
+#include "menu.h"
+
+int main()
+{
+    int check = 0;
+    IntTree *aTree = create_IntTree();
+
+    printf("START!!!");
+
+    do
+    {
+    check = menu1 ();
+
+    switch (check)
+	{
+	case 1 :
+	    insert_Node(aTree);
+    break;
+
+	case 2 :
+	    find_Node(aTree);
+    break;
+
+	case 3 :
+	    delete_Node(aTree);
+    break;
+
+    case 4 :
+	    print_Tree_(aTree);
+    break;
+
+    case 5 :
+	    size_Tree(aTree);
+    break;
+
+    case 6 :
+	    delete_Tree(aTree);
+    break;
+	}
+
+    }while(check !=6);
+
+	The_End();
+
+    return 0;
+}
