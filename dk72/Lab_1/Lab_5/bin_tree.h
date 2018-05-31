d46d7877de2e0cb056c087c4362a61c206d1cd2c
AlexPays <32728226+alexpays99@users.noreply.github.com>
2018-06-01 01:33:07 +0300

    Update bin_tree.h

--- dk72/Lab_1/Lab_5/bin_tree.h
+++ dk72/Lab_1/Lab_5/bin_tree.h
@@ -1,17 +1,14 @@
-//
-// Created by vadim on 20.05.2018.
-//
-
 #ifndef bin_tree_h
 #define bin_tree_h
-typedef struct __tagNode
+
+typedef struct tagNode
 {
     int value;
-    struct __tagNode *leftChild;
-    struct __tagNode *rightChild;
+    struct tagNode *leftChild;
+    struct tagNode *rightChild;
 } Node;
 
-typedef struct __tagTree
+typedef struct tagTree
 {
     Node *root;
     int count;
