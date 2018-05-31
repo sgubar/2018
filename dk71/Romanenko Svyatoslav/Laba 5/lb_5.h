4707816ed085e86c1487af62ea72071852c352cf
Slavaromanenko3424 <36336001+Slavaromanenko3424@users.noreply.github.com>
2018-05-31 19:40:28 +0300

    Add files via upload

---
+++ dk71/Romanenko Svyatoslav/Laba 5/lb_5.h
@@ -0,0 +1,30 @@
+#ifndef LB_5_H
+#define LB_5_H
+typedef struct __tagNode
+{
+    float value;
+    struct __tagNode *leftChild;
+    struct __tagNode *rightChild;
+} FloatNode;
+
+typedef struct __tagTree
+{
+    FloatNode *root;
+    float count;
+} FloatTree;
+
+void directOrder(FloatNode *aNode);
+void printTree (FloatTree *aTree);
+
+FloatTree *createFloatTree();
+void destroyFloatTree(FloatTree *aTree);
+
+void insertFloatValueToTree(FloatTree *aTree, float aValue);
+
+FloatNode *findNodeWithValue(FloatTree *aTree, float aValue);
+
+
+FloatNode* FindMin(FloatNode* aNode);
+FloatNode* deleteNodeWithElement(FloatTree *aTree,FloatNode* aNode, float aElement);
+
+#endif
