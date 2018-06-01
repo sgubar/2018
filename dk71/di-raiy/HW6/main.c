6305de1d23e693997907747f8467ae4b4bf0c083
di-raiy <36276100+di-raiy@users.noreply.github.com>
2018-06-01 10:13:16 +0300

    Add files via upload

---
+++ dk71/di-raiy/HW6/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools.h"
+
+int main()
+{
+	List *thelist = create_list();
+
+	int check = 0;
+	char letter;
+	int index = 0;
+	printf("\n\tAll operation:");
+	printf("\n 1.---size");
+	printf("\n 2.---print");
+	printf("\n 3.---delete list");
+	printf("\n 4.---add element");
+	printf("\n 5.---delete element");
+	printf("\n 6.---insert element");
+	printf("\n 7.---sort element");
+	printf("\n 8.---exit");
+
+
+	while(1)
+    {
+
+	    scanf("%d", &check);
+
+	     switch(check)
+		{
+		 case 1:
+			size_list(thelist);
+			     break;
+
+		 case 2:
+
+			 print_list(thelist);
+			     break;
+
+		 case 3:
+
+			delete_list(thelist);
+			printf("\n List was deleted");
+			     break;
+
+		 case 4:
+
+			printf("\n Enter the letter:");
+			scanf("%c", &letter);
+            add_element(thelist, letter);
+			     break;
+
+	  	 case 5:
+
+			printf("\n Enter the letter :");
+			scanf("%d", &index);
+            delete_element(thelist, index);
+			     break;
+
+
+		 case 6:
+
+			printf("\n Enter the letter :");
+			scanf("%c", &letter);
+			printf("\n Enter the index :");
+			scanf("%d", &index);
+            insert_element(thelist, index, letter);
+			     break;
+
+		 case 7:
+
+			qsort_list(thelist, 0, thelist->size - 1);
+			     break;
+
+		 case 8:
+
+			return 0;
+		}
+
+	}
+}
---
+++ dk71/di-raiy/HW6/tools.h
@@ -0,0 +1,38 @@
+#ifndef tools
+#define tools
+#include <stdio.h>
+
+
+typedef struct intNode Node;
+
+typedef struct intNode
+{
+  char letter;
+  Node *prev;
+  Node *next;
+}Node;
+
+typedef struct intList
+{
+  int size;
+  Node *first;
+  Node *last;
+}List;
+
+List *create_list();
+void size_list(List *thelist);
+void print_list(List *thelist);
+void delete_list(List *thelist);
+Node *createNodeWithLetter(char letter);
+void add_element(List *thelist, char letter);
+void delete_element(List *thelist, int index);
+void qsort_list(List *thelist, int first, int last);
+void insert_element(List *thelist, int index, char letter);
+Node *NodeAtIndex(List *thelist, int index);
+void swap(List *thelist, int theLeft, int theRight);
+int partition(List *thelist, int first, int last);
+void qsort_list(List *thelist, int first, int last);
+
+
+#endif
+
