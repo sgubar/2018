14dcc6838dcafd67edf1c43f2b84aa2afc0e56f2
di-raiy <36276100+di-raiy@users.noreply.github.com>
2018-06-01 10:06:03 +0300

    Add files via upload

---
+++ dk71/di-raiy/HW5/dk_triangle_array.c
@@ -0,0 +1,144 @@
+#include "dk_triangle_array.h"
+#include <stdlib.h>
+#include "dk_triangle2.h"
+
+
+triangleArray *createArray(int aNumber)
+{
+	triangleArray *theResult = NULL;
+
+	if (aNumber > 0)
+	{
+		theResult = (triangleArray *)malloc(sizeof(triangleArray));
+		theResult->flat = malloc(sizeof(float)*aNumber);
+		if (NULL != theResult)
+		{
+			theResult->triangles = (triangle **)malloc(sizeof(triangle *)*aNumber);
+
+			if (NULL != theResult->triangles)
+			{
+				theResult->number = aNumber;
+				theResult->count = 0;
+			}
+			else
+			{
+				free(theResult);
+				theResult = NULL;
+			}
+		}
+	}
+
+	return theResult;
+}
+
+void freeArray(triangleArray *anArray)
+{
+	int i;
+	if (NULL != anArray)
+	{
+		for (i = 0; i < anArray->count; i ++)
+		{
+			destroy_triangle(anArray->triangles[i]);
+		}
+
+		free(anArray->triangles);
+		free(anArray);
+	}
+}
+
+
+int addElement(triangleArray *anArray, triangle *atriangle, float flat)
+{
+	int theResult = -1;
+
+	if (NULL != anArray && NULL != atriangle &&	anArray->count < anArray->number)
+	{
+		anArray->triangles[anArray->count] =
+					create_triangle(atriangle->A, atriangle->B, atriangle->C);
+		anArray->flat[anArray->count] = flat;
+		theResult = anArray->count;
+		anArray->count ++;
+	}
+
+	return theResult;
+}
+
+void writeArrayToJSON(FILE *aFile, triangleArray *anArray)
+{
+	int i;
+	if (NULL == anArray || NULL == aFile)
+	{
+		return ;
+	}
+
+	fprintf(aFile, "{\n\"number\" : %d,\n\"count\" : %d,\n",
+			anArray->number, anArray->count);
+	fprintf(aFile, "\"triangles\" : \n[\n");
+
+	for (i = 0; i < anArray->count; i ++)
+	{
+		writetriangleToJSON(aFile, anArray->triangles[i]);
+
+		if (i < anArray->count - 1)
+		{
+			fprintf(aFile, ",");
+		}
+		fprintf(aFile, "\n");
+	}
+
+	fprintf(aFile, "]\n}");
+}
+
+
+int line_search(triangleArray *anArray, float key)
+{
+	int i;
+	int pos;
+    for(i = 0; i < anArray->count; i++)
+    {
+         if (flat_triangle(anArray->triangles[i]) == key)
+        {
+		printf("\n\n\tFIND!\n");
+		pos=i;
+		return pos+1;
+		}
+    }
+    return -1;
+}
+
+
+
+void quick_sort(triangleArray *anArray, int first, int last)
+{
+int mid, left, right;
+
+  left = first;
+  right = last;
+
+  mid = anArray->flat[(left + right) / 2];
+  while (left <= right)
+  {
+    while (anArray->flat[left] < mid) left++;
+
+    while (anArray->flat[right] > mid) right--;
+
+    if (left <= right)
+	{
+        swap(&anArray->triangles[left], &anArray->triangles[right]);
+        left++;
+        right--;
+	}
+  }
+  if (first < right) quick_sort(anArray, first, right);
+
+  if (left < last) quick_sort(anArray, left, last);
+
+}
+
+
+void swap(int *one, int *two)
+{
+	int three = *one;
+	*one = *two;
+	*two = three;
+}
---
+++ dk71/di-raiy/HW5/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "dk_triangle2.h"
+#include "dk_triangle_array.h"
+
+int main(int argc, const char * argv[])
+{
+	int k;
+	int i;
+	int A_x, A_y, B_x, B_y, C_x, C_y;
+	float search;
+	int position;
+	triangleArray *theArray = createArray(5);
+	printf("Enter the number of triangles\t");
+	scanf("%d", &k);
+	for(i=1; i<=k; i++)
+	 {
+	 	printf("\n\tEnter %d triangle\n", i);
+
+	    enter(&A_x,&A_y);
+	 	enter(&B_x,&B_y);
+	 	enter(&C_x,&C_y);
+
+		Point A = {A_x,A_y};
+		Point B = {B_x,B_y};
+		Point C = {C_x,C_y};
+
+		triangle ABC = {&A, &B, &C};
+
+		addElement(theArray, &ABC, flat_triangle(&ABC));
+	 }
+
+
+	FILE *theFile = fopen("D:/test.json", "a");
+
+	writeArrayToJSON(theFile, theArray);
+
+	fflush(theFile);
+	fclose(theFile);
+
+
+	for(i=0; i<theArray->count; i++)
+	{
+		printf("%f\n", theArray->flat[i]);
+	}
+
+	quick_sort(theArray, 0, theArray->count-1);
+
+
+	FILE *theFile2 = fopen("D:/test2.json", "a");
+
+	writeArrayToJSON(theFile2, theArray);
+
+	fflush(theFile2);
+	fclose(theFile2);
+
+	while(1)
+		{
+		printf("\tEnter the flat of triangle:");
+		scanf("\n%f", &search);
+		position = line_search(theArray, search);
+		printf("\nPosition of a triangle = %d\n\n", position);
+		}
+
+	freeArray(theArray);
+
+	return 0;
+}
