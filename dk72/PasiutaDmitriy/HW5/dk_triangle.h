b337ad9c9e21c82500a491e338db34ca964778db
pasiuta <36278845+pasiuta@users.noreply.github.com>
2018-06-01 11:55:55 +0300

    Create dk_triangle.h

---
+++ dk72/PasiutaDmitriy/HW5/dk_triangle.h
@@ -0,0 +1,48 @@
+//
+// Created by dima on 17.05.18.
+//
+
+#ifndef HW4_DK_TRIANGLE_ARRAY_H
+#define HW4_DK_TRIANGLE_ARRAY_H
+
+
+
+#include <stdio.h>
+#include "dk_tool.h"
+
+typedef struct tagTriangleArray
+{
+    int number;
+    int count;
+    Triangle **triangles;
+}TriangleArray;
+
+/*
+{
+	"number" : (int),
+	"count" : (int),
+	"lines" :
+	[
+ 		 {
+ 			"A" : {"x" : (int), "y" : (int)},
+		 	"B" : {"x" : (int), "y" : (int)}
+		 },
+ },
+ 		{},
+ 		....
+	]
+}
+*/
+
+// init/dealloc
+TriangleArray *createArray(int aNumber);
+void freeArray(TriangleArray *anArray);
+
+//interface
+// returns index of added element OR -1 if fail
+int addElement(TriangleArray *anArray, Triangle *atriangle);
+void writeArrayToJSON(FILE *aFile, TriangleArray *anArray);
+void quickSort(TriangleArray *anArray, int aLeftIndex, int aRightIndex);
+int linearSearch(TriangleArray *anArray, float value, int size);
+
+#endif //HW4_DK_TRIANGLE_ARRAY_H
