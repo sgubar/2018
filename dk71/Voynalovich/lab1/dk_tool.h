fff8e0204ec97197daf925d774cfc1911db037e0
Voinstar <36274842+Voinstar@users.noreply.github.com>
2018-06-07 13:19:47 +0300

    Add files via upload

---
+++ dk71/Voynalovich/lab1/dk_tool.h
@@ -0,0 +1,42 @@
+//
+
+//Lab_Work_1_Functions.h
+
+//
+
+//HW1
+
+//
+
+//Created by Yan Latyshev on 2/13/18.
+
+//
+
+//Copyright © 2018 Yan Latyshev.
+
+
+
+
+
+
+
+
+
+#ifndef Lab_Work_1_2_Functions_H
+
+#define Lab_Work_1_2_Functions_H
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+int factorial (int value);
+
+int power(int base, int n);
+
+
+
+#endif	
