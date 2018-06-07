226bd4bf84aed338ad6db9939a2d44e08952719a
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-06-07 12:36:47 +0300

    Update main.c

--- dk72/Sarazhinsky Valentin/labki/lab1/main.c
+++ dk72/Sarazhinsky Valentin/labki/lab1/main.c
@@ -8,9 +8,24 @@ int main(int argc, char *argv[]) {
 	scanf("%d",&a);
 	printf("enter value B:");
 	scanf("%d",&b);
+	while(b==0){
+		printf("a value B can not be 0!!!\n");
+		printf("enter value B:");
+		scanf("%d",&b);
+	}
 	printf("enter value C:");
 	scanf("%d",&c);
+	while(c<=0){
+		printf("C must be >0 !!!\n");
+		printf("enter value C:");
+		scanf("%d",&c);
+	}
+	while((b+c)<0){
+		printf("B+C must be >=0 !!!\n");
+		printf("enter value C:");
+		scanf("%d",&c);
+	}
 	float theResult=multiply(a,division(sqrt(b+c),b))+math_sum(c,a);
-	printf("%f \n",theResult);
+	printf("Q = %f \n",theResult);
 	return 0;
 }
