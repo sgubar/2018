10e46869c6c784aabf32ef98854a3211ed79c7b3
yaroslav-dzhur <32819604+yaroslav-dzhur@users.noreply.github.com>
2018-06-07 07:19:18 +0300

    Delete dz4.h

--- dk71/Yaroslav Dzhur/DZ4/dz4.h
+++
@@ -1,9 +0,0 @@
-#ifndef dz4_h
-#define dz4_h
-
-#include <stdlib.h>
-#include <stdio.h>
-#include "dz3.h"
-void insertion_sort(TriangleArray *theArray, FILE *file);
-void printTriangle(TriangleArray *theArray);
-#endif
