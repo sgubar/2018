81f3576b6c45725df585a741ceb02682d2972bc2
syabrenko <36296755+syabrenko@users.noreply.github.com>
2018-06-07 12:14:41 +0300

    Create tree.h

---
+++ dk72/AlexandraSyabrenko/Lab5/tree.h
@@ -0,0 +1,32 @@
+#ifndef tree_h
+#define tree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node Node;
+
+struct node
+{
+    double value;
+    Node *leftChild;
+    Node *rightChild;
+};
+
+typedef struct tree
+{
+    Node *root;
+    double count;
+}Tree;
+
+Tree *createTree(void);
+void deleteTree(Tree *aTree);
+void printTree(Node *aRoot);
+
+void insertDoubleValueToTree(Tree *aTree, double aValue);
+int deleteValueFromTree(Tree *aTree, double aValue);
+
+Node *searchNodeWithValue(Tree *aTree, double aValue);
+
+#endif /* tree_h */
