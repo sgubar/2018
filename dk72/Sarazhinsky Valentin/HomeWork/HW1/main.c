c7ead3ad6ea12e03860c9e0f337c7bdfd7f14841
valentin5135 <33522860+valentin5135@users.noreply.github.com>
2018-06-07 12:25:24 +0300

    Update main.c

--- dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
+++ dk72/Sarazhinsky Valentin/HomeWork/HW1/main.c
@@ -2,23 +2,18 @@
 #include <stdlib.h>
 #include "dk_tool.h"
 
- 
+ 	int *matrix, *trans_matrix, lines, columns;
 int main(int argc, char *argv[]){
  
- 	int *matrix, *trans_matrix, lines, columns;
+ 
  	
 	printf("enter the length of the lines:");
 	scanf("%d",&lines);
 	printf("enter the length of the columns:");
 	scanf("%d",&columns);
 	
-	matrix = (int*)malloc((lines+columns)*sizeof(int));  
-    trans_matrix = (int*)malloc((columns+lines)*sizeof(int));
-	
-	matrix = input_matrix(matrix, lines, columns);
-	trans_matrix = transponation(matrix, trans_matrix, lines, columns);
-	output_matrix(trans_matrix, lines, columns);
-	free(matrix);
-	free(trans_matrix);
+	transponation(input_matrix(lines, columns), lines, columns);
+
+
 	return 0;
 }
