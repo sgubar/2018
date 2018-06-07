cbf389fc920c54276d7a4bf654acec8d4553238d
syabrenko <36296755+syabrenko@users.noreply.github.com>
2018-06-07 12:24:09 +0300

    Create binarytree.h

---
+++ dk72/AlexandraSyabrenko/HW7/binarytree.h
@@ -0,0 +1,35 @@
+#ifndef binarytree_h
+#define binarytree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node Node;
+
+struct node
+{
+    float value;
+    Node *leftChild;
+    Node *rightChild;
+};
+
+typedef struct tree
+{
+    Node *root;
+    float count;
+}Tree;
+
+Tree *createTree(void);
+
+void deleteTree(Tree *aTree);
+
+void printTree(Node *aRoot);
+
+void insertFloatValueToTree(Tree *aTree, float aValue);
+
+int deleteValueFromTree(Tree *aTree, float aValue);
+
+Node *searchNodeWithValue(Tree *aTree, float aValue);
+
+#endif /* binarytree_h */
