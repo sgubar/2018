9fe665c8dadf63d8aa16e08702a8896eb4189371
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:43:48 +0300

    Create quickSort.c

---
+++ dk71/Masha/HW6/quickSort.c
@@ -0,0 +1,48 @@
+#include "quickSort.h"
+#include "dk_node.h"
+
+int partitionIt(IntList *aList, int aLeftIndex, int aRightIndex, int aPivot)
+{
+	int theLeft = aLeftIndex - 1;
+	int theRight = aRightIndex + 1;
+
+	while (1)
+	{
+
+		while (theLeft < aRightIndex && SLNodeAtIndex(aList, ++theLeft
+		)->value < aPivot);
+
+
+		while (theRight > aLeftIndex && SLNodeAtIndex(aList,--theRight)->value > aPivot);
+
+		if (theLeft >= theRight)
+		{
+			break;
+		}
+		else
+		{
+
+
+			SLSwapNodesByIndex(aList, theLeft, theRight);
+		}
+	}
+
+	return theLeft;
+}
+
+
+void quickSort(IntList *aList, int aLeftIndex, int aRightIndex)
+{
+	if (aRightIndex - aLeftIndex <= 0)
+	{
+		return;
+	}
+
+	int thePivot = SLNodeAtIndex(aList, aRightIndex)->value;
+	int thePartitionIndex = partitionIt(aList, aLeftIndex, aRightIndex, thePivot);
+
+	quickSort(aList, aLeftIndex, thePartitionIndex - 1);
+
+
+	quickSort(aList, thePartitionIndex + 1, aRightIndex);
+}
