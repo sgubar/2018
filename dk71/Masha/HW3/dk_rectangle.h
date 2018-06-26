7ba40beacbc49fbb2d05307207f9b5e8c6f45a08
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:28:08 +0300

    Create dk_rectangle.h

---
+++ dk71/Masha/HW3/dk_rectangle.h
@@ -0,0 +1,32 @@
+#ifndef dk_rectangle_h
+#define dk_rectangle_h
+
+#include <stdio.h>
+
+
+typedef struct
+{
+	int x;
+	int y;
+}Coordinate;
+
+
+typedef struct tagLine
+{
+	Coordinate *A;
+	Coordinate *B;
+	Coordinate *C;
+	Coordinate *D;
+}Point, *ppoint;
+
+Coordinate *copyCoordinateWithCoordinate(Coordinate *Coordin);
+Point *createRectangle(Coordinate *A, Coordinate *B, Coordinate *C,Coordinate *D);
+void destroyRectangle(Point *aLine);
+float lenghLine(Coordinate *A,Coordinate *B);
+void printRectangle(Point *aPoint);
+//int checktherectangle(Coordinate *A,Coordinate *B,Coordinate *C,Coordinate *D);
+//float Ploscha(Point *Rectan);
+void writeCoordinateToJSON(FILE *aFile, Coordinate *aCoordinate);
+void writeRectangleToJSON(FILE *aFile, Point *aRectangle);
+
+#endif
