8995eea824e739be0ad57786a78ac6a2119654ed
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:41:32 +0300

    Create dk_node.h

---
+++ dk71/Masha/HW6/dk_node.h
@@ -0,0 +1,21 @@
+#ifndef dk_node_h
+#define dk_node_h
+
+#include <stdio.h>
+
+
+typedef struct __IntNode IntNode;
+
+
+struct __IntNode
+{
+	int value;
+	IntNode *nextNode;//<! - the reference to next node
+};
+
+
+IntNode *SLCreateNodeWithIntValue(int aValue);
+
+void SLFreeIntNode(IntNode *aNode);
+
+#endif
