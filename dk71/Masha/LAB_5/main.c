c766366cde56e27d4b88b6a72f9849470a857b18
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:19:31 +0300

    Create main.c

---
+++ dk71/Masha/LAB_5/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"dk_tree.h"
+#include "menu.h"
+
+int main(void)
+{
+
+    char choice;
+
+    DoubleTree *aTree = createDoubleTree();
+    while ((choice = menu()) != 'q')
+    {
+        switch (choice)
+        {
+            case 'a' :  addnumber(aTree);
+                break;
+            case 'l' :  show_tree(aTree);
+                break;
+            case 'f' :  findnumber(aTree);
+                break;
+            case 'n' :  the_number_of_values(aTree);
+
+                break;
+            case 'd' :  delete_a_value(aTree);
+                break;
+            default  :  puts("Switching error");
+        }
+    }
+    destroyDoubleTree(aTree);
+    puts("Bye.");
+
+    return 0;
+}
