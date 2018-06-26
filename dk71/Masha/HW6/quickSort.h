81ada4ae4b8db948a7c27b33a2c4955e0b0f8520
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:44:22 +0300

    Create quickSort.h

---
+++ dk71/Masha/HW6/quickSort.h
@@ -0,0 +1,10 @@
+#ifndef quickSort_h
+#define quickSort_h
+
+#include <stdio.h>
+#include "dk_list.h"
+
+int partitionIt(IntList *aList, int aLeftIndex, int aRightIndex, int aPivot);
+void quickSort(IntList *aList, int aLeftIndex, int aRightIndex);
+
+#endif
