aa4c743a382c55e62e278a87dc9ab2d482853704
maryvkuz <36297355+maryvkuz@users.noreply.github.com>
2018-06-27 01:42:07 +0300

    Create main.c

---
+++ dk71/Masha/HW6/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dk_node.h"
+#include "dk_list.h"
+#include"menu.h"
+#include "quickSort.h"
+
+
+int main(int argc, const char * argv[])
+{
+	doTestSLList();
+
+	return 0;
+}
