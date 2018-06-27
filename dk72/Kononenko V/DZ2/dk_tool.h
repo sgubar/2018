027845f4b9c0940fb91bec954bcfa4a5ccc1ac93
LivesPain <36304357+LivesPain@users.noreply.github.com>
2018-06-27 04:28:37 +0300

    Add files via upload

---
+++ dk72/Kononenko V/DZ2/dk_tool.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdio.h>
+
+typedef struct Point
+{
+	int x;
+	int y;
+}Point;
+
+typedef struct Fig
+{
+	Point *A;
+	Point *B;
+	Point *C;
+}Fig;
+
+typedef struct Param
+{
+	float a;
+	float b;
+	float c;
+	float S;
+}Param;
+
+Fig *createFig(Point *A, Point *B, Point *C);
+
+void destroyFig(Fig *aFig);
+
+Param *enterParam(Fig *aFig);
+
+void printFig(Fig *aFig);
+
+void printParam(Param *aParam);
