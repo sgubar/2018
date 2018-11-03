f654da965b15e46b6d2bd6cfeb1889054e4dc0df
Vadim Kharchuk <vadimharchuk@gmail.com>
2018-11-03 23:56:01 +0200

    oo

--- dk71/Anhelina Moroz/Homework_1/dk_tool.h
+++
@@ -1,9 +0,0 @@
-#ifndef dk_tool
-#define dk_tool
-
-#include <stdio.h>
-
-void subtraction (int size, int matrix_A[size][size], int matrix_B[size][size], int matrix_result[size][size]);
-void scan_matrix (int size, int matrix[size][size]);
-
-#endif
